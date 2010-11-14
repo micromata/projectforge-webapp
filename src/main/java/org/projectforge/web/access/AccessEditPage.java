46b219955d982879fba33b19e28292350b6ed6ac
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2010-11-14 09:59:38 +0000

    PF-90: Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@9261 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/web/access/AccessEditPage.java
@@ -0,0 +1,82 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2010 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.access;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.projectforge.user.GroupDO;
+import org.projectforge.user.GroupDao;
+import org.projectforge.web.wicket.AbstractBasePage;
+import org.projectforge.web.wicket.AbstractEditPage;
+import org.projectforge.web.wicket.EditPage;
+
+@EditPage(defaultReturnPage = AccessListPage.class)
+public class AccessEditPage extends AbstractEditPage<GroupDO, AccessEditForm, GroupDao>
+{
+  private static final long serialVersionUID = 4636922408954211544L;
+
+  private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(AccessEditPage.class);
+
+  @SpringBean(name = "groupDao")
+  private GroupDao groupDao;
+
+  public AccessEditPage(final PageParameters parameters)
+  {
+    super(parameters, "group");
+    super.init();
+  }
+
+  @Override
+  protected AbstractBasePage onSaveOrUpdate()
+  {
+    final Set<Integer> assignedGroupIds = new HashSet<Integer>();
+    for (Integer groupId : form.users.getAssignedValues()) {
+      assignedGroupIds.add(groupId);
+    }
+    groupDao.setAssignedUsers(getData(), assignedGroupIds);
+    return super.onSaveOrUpdate();
+  }
+
+  @Override
+  protected GroupDao getBaseDao()
+  {
+    return groupDao;
+  }
+
+  @Override
+  protected AccessEditForm newEditForm(AbstractEditPage< ? , ? , ? > parentPage, GroupDO data)
+  {
+    return new AccessEditForm(this, data);
+  }
+
+  @Override
+  protected Logger getLogger()
+  {
+    return log;
+  }
+}
---
+++ src/main/java/org/projectforge/web/access/AccessListPage.java
@@ -0,0 +1,165 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2010 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.access;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.projectforge.access.AccessDao;
+import org.projectforge.access.AccessEntryDO;
+import org.projectforge.access.GroupTaskAccessDO;
+import org.projectforge.task.TaskDO;
+import org.projectforge.web.task.TaskEditPage;
+import org.projectforge.web.task.TaskFormatter;
+import org.projectforge.web.wicket.AbstractListPage;
+import org.projectforge.web.wicket.CellItemListener;
+import org.projectforge.web.wicket.CellItemListenerPropertyColumn;
+import org.projectforge.web.wicket.DetachableDOModel;
+import org.projectforge.web.wicket.ListPage;
+import org.projectforge.web.wicket.ListSelectActionPanel;
+import org.projectforge.web.wicket.WebConstants;
+import org.projectforge.web.wicket.WicketLocalizerAndUrlBuilder;
+import org.projectforge.web.wicket.components.SingleImagePanel;
+
+@ListPage(editPage = AccessEditPage.class)
+public class AccessListPage extends AbstractListPage<AccessListForm, AccessDao, GroupTaskAccessDO>
+{
+  private static final long serialVersionUID = 7017404582337466883L;
+
+  @SpringBean(name = "accessDao")
+  private AccessDao accessDao;
+
+  @SpringBean(name = "taskFormatter")
+  private TaskFormatter taskFormatter;
+
+  public AccessListPage(PageParameters parameters)
+  {
+    super(parameters, "access");
+  }
+
+  @SuppressWarnings("serial")
+  @Override
+  protected void init()
+  {
+    final List<IColumn<GroupTaskAccessDO>> columns = new ArrayList<IColumn<GroupTaskAccessDO>>();
+
+    final CellItemListener<GroupTaskAccessDO> cellItemListener = new CellItemListener<GroupTaskAccessDO>() {
+      public void populateItem(Item<ICellPopulator<GroupTaskAccessDO>> item, String componentId, IModel<GroupTaskAccessDO> rowModel)
+      {
+        final GroupTaskAccessDO acces = rowModel.getObject();
+        String cellStyle = null;
+        if (acces.isDeleted() == true) {
+          cellStyle = "text-decoration: line-through;";
+        }
+        if (cellStyle != null) {
+          item.add(new AttributeModifier("style", true, new Model<String>(cellStyle)));
+        }
+      }
+    };
+    columns.add(new CellItemListenerPropertyColumn<GroupTaskAccessDO>(new Model<String>(getString("name")), "name", "name",
+        cellItemListener) {
+      @SuppressWarnings("unchecked")
+      @Override
+      public void populateItem(final Item item, final String componentId, final IModel rowModel)
+      {
+        final GroupTaskAccessDO access = (GroupTaskAccessDO) rowModel.getObject();
+        final TaskDO task = access.getTask();
+        final StringBuffer buf = new StringBuffer();
+        taskFormatter.appendFormattedTask(buf, new WicketLocalizerAndUrlBuilder(getResponse()), task, false, true, false);
+        final Label formattedTaskLabel = new Label(ListSelectActionPanel.LABEL_ID, buf.toString());
+        formattedTaskLabel.setEscapeModelStrings(false);
+        item
+            .add(new ListSelectActionPanel(componentId, rowModel, TaskEditPage.class, task.getId(), AccessListPage.this, formattedTaskLabel));
+        cellItemListener.populateItem(item, componentId, rowModel);
+      }
+    });
+    columns.add(new CellItemListenerPropertyColumn<GroupTaskAccessDO>(new Model<String>(getString("group")), "group.name", "group.name",
+        cellItemListener));
+    columns.add(new CellItemListenerPropertyColumn<GroupTaskAccessDO>(new Model<String>(getString("recursive")), "recursive", "recursive",
+        cellItemListener) {
+      @Override
+      public void populateItem(Item<ICellPopulator<GroupTaskAccessDO>> item, String componentId, IModel<GroupTaskAccessDO> rowModel)
+      {
+        final GroupTaskAccessDO access = (GroupTaskAccessDO) rowModel.getObject();
+        if (access.isRecursive() == true) {
+          item.add(SingleImagePanel.createPresizedImage(componentId, WebConstants.IMAGE_ACCEPT));
+        } else {
+          item.add(createInvisibleDummyComponent(componentId));
+        }
+        cellItemListener.populateItem(item, componentId, rowModel);
+      }
+    });
+    columns.add(new CellItemListenerPropertyColumn<GroupTaskAccessDO>(new Model<String>(getString("access.type")), null, "accessEntries",
+        cellItemListener) {
+      @Override
+      public void populateItem(Item<ICellPopulator<GroupTaskAccessDO>> item, String componentId, IModel<GroupTaskAccessDO> rowModel)
+      {
+        final int rowIndex = ((Item< ? >) item.findParent(Item.class)).getIndex();
+        final GroupTaskAccessDO access = (GroupTaskAccessDO) rowModel.getObject();
+        final List<AccessEntryDO> accessEntries = access.getOrderedEntries();
+        final AccessTablePanel accessTablePanel = new AccessTablePanel(componentId, accessEntries);
+        if (rowIndex == 0) {
+          accessTablePanel.setDrawHeader(true);
+        }
+        item.add(accessTablePanel);
+        accessTablePanel.init();
+        cellItemListener.populateItem(item, componentId, rowModel);
+      }
+    });
+    dataTable = createDataTable(columns, "group.name", true);
+    form.add(dataTable);
+  }
+
+  @Override
+  protected AccessListForm newListForm(AbstractListPage< ? , ? , ? > parentPage)
+  {
+    return new AccessListForm(this);
+  }
+
+  @Override
+  protected AccessDao getBaseDao()
+  {
+    return accessDao;
+  }
+
+  @Override
+  protected IModel<GroupTaskAccessDO> getModel(GroupTaskAccessDO object)
+  {
+    return new DetachableDOModel<GroupTaskAccessDO, AccessDao>(object, getBaseDao());
+  }
+
+  protected AccessDao getAccessDao()
+  {
+    return accessDao;
+  }
+}
