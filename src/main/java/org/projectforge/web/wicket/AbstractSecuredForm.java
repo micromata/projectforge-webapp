91d3800ca8e29322818bd8452628f9e09532f1ba
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2010-11-22 05:56:01 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@9318 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/web/wicket/AbstractSecuredForm.java
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2010 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.wicket;
+
+public abstract class AbstractSecuredForm<F, P extends AbstractSecuredBasePage> extends AbstractForm<F, P>
+{
+  private static final long serialVersionUID = 5034574268522349613L;
+
+  public AbstractSecuredForm(P parentPage)
+  {
+    super(parentPage);
+  }
+
+  /**
+   * @param key
+   * @see AbstractSecuredPage#getUserPrefEntry(String)
+   */
+  public Object getUserPrefEntry(final String key)
+  {
+    return parentPage.getUserPrefEntry(key);
+  }
+
+  /**
+   * @param expectedType
+   * @param key
+   * @see AbstractSecuredPage#getUserPrefEntry(Class, String)
+   */
+  public Object getUserPrefEntry(final Class< ? > expectedType, final String key)
+  {
+    return parentPage.getUserPrefEntry(expectedType, key);
+  }
+
+  /**
+   * @param key
+   * @param value
+   * @param persistent
+   * @see AbstractSecuredPage#putUserPrefEntry(String, Object, boolean)
+   */
+  public void putUserPrefEntry(String key, Object value, boolean persistent)
+  {
+    parentPage.putUserPrefEntry(key, value, persistent);
+  }
+}
