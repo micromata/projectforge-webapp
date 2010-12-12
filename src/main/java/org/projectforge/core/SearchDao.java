49544d5a355bf51ee38fbd976b338251fdfe9a88
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2010-12-12 09:30:35 +0000

    Panta rhei.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@9551 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/core/SearchDao.java
+++ src/main/java/org/projectforge/core/SearchDao.java
@@ -36,41 +36,12 @@ import org.apache.commons.collections.PredicateUtils;
 import org.apache.commons.lang.ClassUtils;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.criterion.Expression;
 import org.hibernate.criterion.Order;
-import org.projectforge.access.AccessDao;
-import org.projectforge.access.GroupTaskAccessDO;
-import org.projectforge.address.AddressDO;
-import org.projectforge.address.AddressDao;
-import org.projectforge.book.BookDO;
-import org.projectforge.book.BookDao;
+import org.hibernate.criterion.Restrictions;
 import org.projectforge.common.NumberHelper;
-import org.projectforge.fibu.KontoDO;
-import org.projectforge.fibu.KontoDao;
-import org.projectforge.fibu.KundeDO;
-import org.projectforge.fibu.KundeDao;
-import org.projectforge.fibu.ProjektDO;
-import org.projectforge.fibu.ProjektDao;
-import org.projectforge.fibu.RechnungDO;
-import org.projectforge.fibu.RechnungDao;
-import org.projectforge.fibu.kost.BuchungssatzDO;
-import org.projectforge.fibu.kost.BuchungssatzDao;
-import org.projectforge.fibu.kost.Kost1DO;
-import org.projectforge.fibu.kost.Kost1Dao;
-import org.projectforge.fibu.kost.Kost2ArtDO;
-import org.projectforge.fibu.kost.Kost2ArtDao;
-import org.projectforge.fibu.kost.Kost2DO;
-import org.projectforge.fibu.kost.Kost2Dao;
-import org.projectforge.task.TaskDO;
-import org.projectforge.task.TaskDao;
 import org.projectforge.task.TaskNode;
 import org.projectforge.task.TaskTree;
 import org.projectforge.timesheet.TimesheetDO;
-import org.projectforge.timesheet.TimesheetDao;
-import org.projectforge.user.GroupDO;
-import org.projectforge.user.GroupDao;
-import org.projectforge.user.PFUserDO;
-import org.projectforge.user.UserDao;
 import org.projectforge.user.UserGroupCache;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.transaction.annotation.Propagation;
@@ -91,216 +62,8 @@ public class SearchDao extends HibernateDaoSupport
 
   private UserGroupCache userGroupCache;
 
-  private TimesheetDao timesheetDao;
-
-  private TaskDao taskDao;
-
-  private BookDao bookDao;
-
-  private AddressDao addressDao;
-
-  private UserDao userDao;
-
-  private GroupDao groupDao;
-
-  private AccessDao accessDao;
-
-  private RechnungDao rechnungDao;
-
-  private BuchungssatzDao buchungssatzDao;
-
-  private Kost1Dao kost1Dao;
-
-  private Kost2Dao kost2Dao;
-
-  private Kost2ArtDao kost2ArtDao;
-
-  private KontoDao kontoDao;
-
-  private KundeDao kundeDao;
-
-  private ProjektDao projektDao;
-
-  public List<List<SearchResultData>> getResultLists(SearchFilter filter)
-  {
-    List<List<SearchResultData>> result = new ArrayList<List<SearchResultData>>();
-    addList(result, getTimesheetList(filter), filter, SearchArea.TIMESHEET);
-    addList(result, getAddressList(filter), filter, SearchArea.ADDRESS);
-    addList(result, getTaskList(filter), filter, SearchArea.TASK);
-    addList(result, getBookList(filter), filter, SearchArea.BOOK);
-    addList(result, getRechnungList(filter), filter, SearchArea.RECHNUNG);
-    addList(result, getUserList(filter), filter, SearchArea.USER);
-    addList(result, getGroupList(filter), filter, SearchArea.GROUP);
-    addList(result, getAccessList(filter), filter, SearchArea.ACCESS);
-    addList(result, getKost2ArtList(filter), filter, SearchArea.KOST2ART);
-    addList(result, getKundeList(filter), filter, SearchArea.KUNDE);
-    addList(result, getProjektList(filter), filter, SearchArea.PROJEKT);
-    addList(result, getKontoList(filter), filter, SearchArea.KONTO);
-    addList(result, getKost1List(filter), filter, SearchArea.KOST1);
-    addList(result, getKost2List(filter), filter, SearchArea.KOST2);
-    addList(result, getBuchungssatzList(filter), filter, SearchArea.BUCHUNGSSATZ);
-    return result;
-  }
-
-  private void addList(List<List<SearchResultData>> lists, List<SearchResultData> list, SearchFilter filter, SearchArea area)
-  {
-    if ((filter.getArea() == SearchArea.ALL || filter.getArea() == area) && list != null && list.size() > 0) {
-      lists.add(list);
-    }
-  }
-
-  public List<SearchResultData> getAddressList(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.ADDRESS) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, AddressDO.class, addressDao);
-    return list;
-  }
-
-  public List<SearchResultData> getTimesheetList(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.TIMESHEET) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, TimesheetDO.class, timesheetDao);
-    return list;
-  }
-
-  public List<SearchResultData> getUserList(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.USER) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, PFUserDO.class, userDao);
-    return list;
-  }
-
-  public List<SearchResultData> getGroupList(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.GROUP) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, GroupDO.class, groupDao);
-    return list;
-  }
-
-  public List<SearchResultData> getAccessList(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.ACCESS) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, GroupTaskAccessDO.class, accessDao);
-    return list;
-  }
-
-  public List<SearchResultData> getTaskList(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.TASK) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, TaskDO.class, taskDao);
-    return list;
-  }
-
-  public List<SearchResultData> getBookList(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.BOOK) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, BookDO.class, bookDao);
-    return list;
-  }
-
-  public List<SearchResultData> getRechnungList(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.RECHNUNG) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, RechnungDO.class, rechnungDao);
-    return list;
-  }
-
-  public List<SearchResultData> getBuchungssatzList(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.BUCHUNGSSATZ) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, BuchungssatzDO.class, buchungssatzDao);
-    return list;
-  }
-
-  public List<SearchResultData> getKost1List(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.KOST1) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, Kost1DO.class, kost1Dao);
-    return list;
-  }
-
-  public List<SearchResultData> getKost2ArtList(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.KOST2ART) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, Kost2ArtDO.class, kost2ArtDao);
-    return list;
-  }
-
-  public List<SearchResultData> getKost2List(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.KOST2) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, Kost2DO.class, kost2Dao);
-    return list;
-  }
-
-  public List<SearchResultData> getKontoList(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.KONTO) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, KontoDO.class, kontoDao);
-    return list;
-  }
-
-  public List<SearchResultData> getKundeList(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.KUNDE) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, KundeDO.class, kundeDao);
-    return list;
-  }
-
-  public List<SearchResultData> getProjektList(SearchFilter filter)
-  {
-    if (filter.getArea() != SearchArea.ALL && filter.getArea() != SearchArea.PROJEKT) {
-      return null;
-    }
-    Session session = getSession();
-    List<SearchResultData> list = getHistoryEntries(session, filter, ProjektDO.class, projektDao);
-    return list;
-  }
-
   @SuppressWarnings("unchecked")
-  private List<SearchResultData> getHistoryEntries(Session session, SearchFilter filter, Class clazz, BaseDao baseDao)
+  public List<SearchResultData> getHistoryEntries(Session session, SearchFilter filter, Class clazz, BaseDao baseDao)
   {
     if (filter == null || filter.getMaxRows() == null) {
       log.info("Filter or rows in filter is null (may be Search as redirect after login): " + filter);
@@ -314,16 +77,16 @@ public class SearchDao extends HibernateDaoSupport
     // First get all history entries matching the filter and the given class.
     String className = ClassUtils.getShortClassName(clazz);
     Criteria crit = session.createCriteria(HistoryEntry.class);
-    crit.add(Expression.eq("className", className));
+    crit.add(Restrictions.eq("className", className));
     if (filter.getStartTime() != null && filter.getStopTime() != null) {
-      crit.add(Expression.between("timestamp", filter.getStartTime(), filter.getStopTime()));
+      crit.add(Restrictions.between("timestamp", filter.getStartTime(), filter.getStopTime()));
     } else if (filter.getStartTime() != null) {
-      crit.add(Expression.ge("timestamp", filter.getStartTime()));
+      crit.add(Restrictions.ge("timestamp", filter.getStartTime()));
     } else if (filter.getStopTime() != null) {
-      crit.add(Expression.le("timestamp", filter.getStopTime()));
+      crit.add(Restrictions.le("timestamp", filter.getStopTime()));
     }
     if (filter.getModifiedByUserId() != null) {
-      crit = crit.add(Expression.eq("userName", filter.getModifiedByUserId().toString()));
+      crit = crit.add(Restrictions.eq("userName", filter.getModifiedByUserId().toString()));
     }
     crit = crit.setCacheable(true);
     crit.setCacheRegion("historyItemCache");
@@ -341,12 +104,12 @@ public class SearchDao extends HibernateDaoSupport
       ids.add(entry.getEntityId());
     }
     crit = session.createCriteria(clazz);
-    crit.add(Expression.in("id", ids));
+    crit.add(Restrictions.in("id", ids));
     if (filter.getTaskId() != null && (clazz.equals(TimesheetDO.class) == true)) {
       TaskNode node = taskTree.getTaskNodeById(filter.getTaskId());
       List<Integer> taskIds = node.getDescendantIds();
       taskIds.add(node.getId());
-      crit.add(Expression.in("task.id", taskIds));
+      crit.add(Restrictions.in("task.id", taskIds));
     }
     List<ExtendedBaseDO> objects = crit.list();
     // Put all found objects in a map for faster access:
@@ -387,16 +150,6 @@ public class SearchDao extends HibernateDaoSupport
     return result;
   }
 
-  public void setTimesheetDao(TimesheetDao timesheetDao)
-  {
-    this.timesheetDao = timesheetDao;
-  }
-
-  public void setAddressDao(AddressDao addressDao)
-  {
-    this.addressDao = addressDao;
-  }
-
   public void setUserGroupCache(UserGroupCache userGroupCache)
   {
     this.userGroupCache = userGroupCache;
@@ -405,70 +158,5 @@ public class SearchDao extends HibernateDaoSupport
   public void setTaskTree(TaskTree taskTree)
   {
     this.taskTree = taskTree;
-  }
-
-  public void setTaskDao(TaskDao taskDao)
-  {
-    this.taskDao = taskDao;
-  }
-
-  public void setBookDao(BookDao bookDao)
-  {
-    this.bookDao = bookDao;
-  }
-
-  public void setUserDao(UserDao userDao)
-  {
-    this.userDao = userDao;
-  }
-
-  public void setGroupDao(GroupDao groupDao)
-  {
-    this.groupDao = groupDao;
-  }
-
-  public void setAccessDao(AccessDao accessDao)
-  {
-    this.accessDao = accessDao;
-  }
-
-  public void setRechnungDao(RechnungDao rechnungDao)
-  {
-    this.rechnungDao = rechnungDao;
-  }
-
-  public void setKost2ArtDao(Kost2ArtDao kost2ArtDao)
-  {
-    this.kost2ArtDao = kost2ArtDao;
-  }
-
-  public void setKundeDao(KundeDao kundeDao)
-  {
-    this.kundeDao = kundeDao;
-  }
-
-  public void setKontoDao(KontoDao kontoDao)
-  {
-    this.kontoDao = kontoDao;
-  }
-
-  public void setBuchungssatzDao(BuchungssatzDao buchungssatzDao)
-  {
-    this.buchungssatzDao = buchungssatzDao;
-  }
-
-  public void setKost1Dao(Kost1Dao kost1Dao)
-  {
-    this.kost1Dao = kost1Dao;
-  }
-
-  public void setKost2Dao(Kost2Dao kost2Dao)
-  {
-    this.kost2Dao = kost2Dao;
-  }
-
-  public void setProjektDao(ProjektDao projektDao)
-  {
-    this.projektDao = projektDao;
   }
 }
