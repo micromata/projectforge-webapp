bd5aed8b73460f0b244d62272e3f2e0ba8a6468c
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2010-12-14 19:58:41 +0000

    Using now Wicket i18n mechanism at default.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@9577 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/web/wicket/MyLocalizer.java
+++ src/main/java/org/projectforge/web/wicket/MyLocalizer.java
@@ -2,13 +2,11 @@ package org.projectforge.web.wicket;
 
 import java.util.Locale;
 import java.util.MissingResourceException;
-import java.util.ResourceBundle;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.wicket.Component;
 import org.apache.wicket.Localizer;
 import org.apache.wicket.model.IModel;
-import org.projectforge.user.PFUserContext;
 
 import de.micromata.genome.gwiki.model.GWikiWeb;
 import de.micromata.genome.gwiki.page.impl.GWikiI18nElement;
@@ -50,10 +48,6 @@ public class MyLocalizer extends Localizer
   @Override
   public String getString(String key, Component component, IModel< ? > model, String defaultValue) throws MissingResourceException
   {
-    final ResourceBundle bundle = PFUserContext.getResourceBundle();
-    if (bundle.containsKey(key) == true) {
-      return bundle.getString(key);
-    }
     final String ret = getI18NFromGWiki(key, component, model);
     if (ret != null) {
       return ret;
