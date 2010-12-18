f557f5b34a8a388d6d64e80def79cba4654c16cd
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2010-12-18 01:27:57 +0000

    Warning removed.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@9660 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/web/wicket/AbstractBasePage.java
+++ src/main/java/org/projectforge/web/wicket/AbstractBasePage.java
@@ -41,7 +41,6 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.image.ContextImage;
 import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.protocol.http.WebRequest;
 import org.apache.wicket.protocol.http.WebResponse;
