b6f26e7dace0880da822ca1b0b2aababd43e87dc
markus <markus@be79319d-3443-0410-9b0a-b0c1b946f33b>
2010-12-20 00:49:11 +0000

    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@9698 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/webapp/scripts/projectforge.js
+++ src/main/webapp/scripts/projectforge.js
@@ -29,65 +29,22 @@ function preventBubble(e) {
 }
 function initTooltips() {
 	// Enable all title attributes as tooltip:
-	$(':*[title]').tooltip( {
-		track : true,
-		delay : 1000,
-		showURL : false,
-		opacity : 1,
-		showBody : " - ",
-		top : 5,
-		left : 5
-	});
+//	$(':*[title]').tooltip( {
+//		track : true,
+//		delay : 1000,
+//		showURL : false,
+//		opacity : 1,
+//		showBody : " - ",
+//		top : 5,
+//		left : 5
+//	});
 }
 function showBookmark() {
 	$("#bookmark").toggle("normal");
 }
 
+
+
+
 var suppressRowClick = 'false';
 
-// Begin: Functionality for DropDownMenu
-var timeout = 500;
-var closetimer = 0;
-var ddmenuitem = 0;
-
-// open hidden layer
-function mopen(id) {
-	// cancel close timer
-	mcancelclosetime();
-
-	// close old layer
-	if (ddmenuitem)
-		ddmenuitem.style.visibility = 'hidden';
-
-	// get new layer and show it
-	ddmenuitem = document.getElementById(id);
-	ddmenuitem.style.visibility = 'visible';
-
-}
-// close showed layer
-function mclose() {
-	if (ddmenuitem)
-		ddmenuitem.style.visibility = 'hidden';
-}
-
-// go close timer
-function mclosetime() {
-	closetimer = window.setTimeout(mclose, timeout);
-}
-
-// cancel close timer
-function mcancelclosetime() {
-	if (closetimer) {
-		window.clearTimeout(closetimer);
-		closetimer = null;
-	}
-}
-
-function openNewWindow(theURL, winName, features) { // v2.0
-	var w = window.open(theURL, winName, features);
-	w.focus();
-}
-
-// close layer when click-out
-document.onclick = mclose;
-// End: Functionality for DropDownMenu
