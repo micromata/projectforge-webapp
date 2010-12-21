88c38e94fe2fcf0e56d090def48187135cc3b319
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2010-12-21 23:10:56 +0000

    Schwarzarbeit.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@9740 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/webapp/scripts/menu.js
+++ src/main/webapp/scripts/menu.js
@@ -1,24 +1,22 @@
 $(function() {
-  // hier verknüpfe ich die listen und konfiguriere die
-  // sortable-funktion.
-  $( "#normal #nav ul li" ).draggable({
-    connectToSortable: "#personal",
-    placeholder: "ui-state-highlight",
-    helper: 'clone'
-  }).disableSelection();
-  
-  $("#trash").droppable({
-		accept: "#personal > li",
-		activeClass: "ui-state-highlight",
-		drop: function( event, ui ) {
-			ui.item.remove;
-		}
-  });
+	// hier verknüpfe ich die listen und konfiguriere die sortable-funktion.
+	$("#normal #nav ul li").draggable( {
+		connectToSortable : "#personal",
+		placeholder : "ui-state-highlight",
+		helper : 'clone'
+	}).disableSelection();
 
-  $( "#personal, #trash").sortable({
-    placeholder: "ui-state-highlight",
-    remove: function(event, ui) {
-    	ui.item.remove();
-    }
-  }).disableSelection();
+	$("#personal").sortable( {
+		placeholder : "ui-state-highlight"
+	}).disableSelection();
+
+	// @kai: Das ist neu, beim Laden muss man natürlich das Sortable disablen...
+	$("#personal, #nav ul").sortable("disable");
+
+	// @kai: dies aktiviert die remover
+	$('.remover').live('click', function() {
+		$(this).parents("li").remove();
+		return false;
+	});
+
 });
