d11bc02799112c6cd15cdc3e9557735bccc38445
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2010-12-24 13:32:53 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@9779 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/web/wicket/layout/AbstractRenderer.java
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2010 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.wicket.layout;
+
+import java.io.Serializable;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+
+/**
+ * Base class for renderers of data objects. This renderer can be re-used by different pages (mobile pages as well as read-only or edit form
+ * pages).
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ * 
+ */
+public abstract class AbstractRenderer implements Serializable
+{
+  private static final long serialVersionUID = -5202334758019046183L;
+
+  protected LayoutContext layoutContext;
+
+  protected WebMarkupContainer container;
+
+  public AbstractRenderer(final WebMarkupContainer container, final LayoutContext layoutContext)
+  {
+    this.layoutContext = layoutContext;
+    this.container = container;
+  }
+
+  public abstract void add();
+
+  protected AbstractRenderer add(final Component component)
+  {
+    container.add(component);
+    return this;
+  }
+
+  protected String getString(final String i18nKey)
+  {
+    return container.getString(i18nKey);
+  }
+}
---
+++ src/main/java/org/projectforge/web/wicket/layout/LayoutContext.java
@@ -0,0 +1,111 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2010 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.wicket.layout;
+
+import java.io.Serializable;
+
+import org.apache.wicket.Session;
+import org.projectforge.web.wicket.AbstractEditForm;
+import org.projectforge.web.wicket.MySession;
+
+/**
+ * The layout context contains information important for the render process. Has a page to be rendered as a read-only page or as a mobile
+ * page? This context is given to several render methods.
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ * 
+ */
+public class LayoutContext implements Serializable
+{
+  private static final long serialVersionUID = -679521117659235197L;
+
+  private boolean mobile;
+
+  private boolean readonly;
+
+  private boolean newObject;
+
+  /**
+   * Sets the mobile flag automatically from MySession.
+   * @see MySession#isMobileUserAgent()
+   * @see Session#get()
+   */
+  public LayoutContext()
+  {
+    mobile = ((MySession) Session.get()).isMobileUserAgent();
+  }
+
+  public LayoutContext(final AbstractEditForm< ? , ? > form)
+  {
+    this();
+    newObject = form.isNew();
+  }
+
+  public boolean isMobile()
+  {
+    return mobile;
+  }
+
+  /**
+   * @param mobile
+   * @return this for chaining.
+   */
+  public LayoutContext setMobile(boolean mobile)
+  {
+    this.mobile = mobile;
+    return this;
+  }
+
+  public boolean isReadonly()
+  {
+    return readonly;
+  }
+
+  /**
+   * @param readonly
+   * @return this for chaining.
+   */
+  public LayoutContext setReadonly(boolean readonly)
+  {
+    this.readonly = readonly;
+    return this;
+  }
+
+  /**
+   * @return true, if this object is rendered as a new object (normally to insert or to create).
+   */
+  public boolean isNew()
+  {
+    return newObject;
+  }
+
+  /**
+   * @param newObject
+   * @return this for chaining.
+   */
+  public LayoutContext setNew(boolean newObject)
+  {
+    this.newObject = newObject;
+    return this;
+  }
+}
