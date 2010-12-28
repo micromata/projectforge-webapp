746a515e474b284f1d61ca9ca2a5156c8a8fc139
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2010-12-28 04:27:37 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@9848 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/web/mobile/JQueryButtonPanel.java
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2010 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.mobile;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.behavior.SimpleAttributeModifier;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * A jquery button with an icon (e. g. at the top right corner).
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ * 
+ */
+public class JQueryButtonPanel extends Panel
+{
+  private static final long serialVersionUID = 6460153798143225741L;
+
+  public JQueryButtonPanel(final String id, final JQueryButtonType type, final Class< ? extends WebPage> pageClass, final String label)
+  {
+    this(id, type, pageClass, null, label);
+  }
+
+  public JQueryButtonPanel(final String id, final JQueryButtonType type, final Class< ? extends WebPage> pageClass,
+      final PageParameters params, final String label)
+  {
+    super(id);
+    final BookmarkablePageLink<String> link;
+    if (params == null) {
+      link = new BookmarkablePageLink<String>("button", pageClass);
+    } else {
+      link = new BookmarkablePageLink<String>("button", pageClass, params);
+    }
+    if (type == JQueryButtonType.PLUS) {
+      link.add(new SimpleAttributeModifier("data-icon", "plus"));
+    }
+    add(new SimpleAttributeModifier("class", "ui-btn-right"));
+    add(link);
+    link.add(new Label("label", label));
+  }
+}
