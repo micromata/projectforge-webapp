e2b2fcd2e46f9cb78ebcce4d1ffc883ff94ae954
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-01-12 18:58:01 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10009 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/web/core/MenuSuffixLabel.java
@@ -0,0 +1,85 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2011 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.core;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+
+/**
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ */
+public class MenuSuffixLabel extends Label
+{
+  private int counter = -1;
+
+  private IModel<Integer> counterModel;
+
+  private static final long serialVersionUID = 3239158008743709277L;
+
+  @SuppressWarnings("serial")
+  MenuSuffixLabel(final IModel<Integer> counterModel)
+  {
+    super("suffix");
+    this.counterModel = counterModel;
+    setDefaultModel(new Model<String>() {
+      @Override
+      public String getObject()
+      {
+        return getCounterValue();
+      }
+    });
+  }
+
+  private String getCounterValue()
+  {
+    if (counterModel == null) {
+      return "";
+    }
+    if (counter < 0) {
+      counter = counterModel.getObject();
+    }
+    if (counter > 0) {
+      return String.valueOf(counter);
+    } else {
+      return "";
+    }
+  }
+
+  @Override
+  protected void onBeforeRender()
+  {
+    counter = -1; // Force recalculation.
+    super.onBeforeRender();
+  }
+
+  @Override
+  public boolean isVisible()
+  {
+    if (counter < 0) {
+      getCounterValue();
+    }
+    return counter > 0;
+  }
+}
