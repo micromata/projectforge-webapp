1bf5dd437140b38ee09e999099183e538edf3a5d
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-01-19 22:42:00 +0000

    alert message after serialize.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10131 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/webapp/scripts/menu.js
+++ src/main/webapp/scripts/menu.js
@@ -1,64 +1,61 @@
 $(function() {
-  // hier verknüpfe ich die listen und konfiguriere die sortable-funktion.
-  $("#normal #nav ul li").draggable( {
-    connectToSortable : "#personal",
-    placeholder : "ui-state-highlight",
-    helper : 'clone'
-  }).disableSelection();
+	// hier verknüpfe ich die listen und konfiguriere die sortable-funktion.
+	$("#normal #nav ul li").draggable( {
+		connectToSortable : "#personal",
+		placeholder : "ui-state-highlight",
+		helper : 'clone'
+	}).disableSelection();
 
-  $("#personal").sortable( {
-    placeholder : "ui-state-highlight",
-    change : function(event, ui) {
-      $(".remover").remove();
-      $("ul#personal li a").prepend('<span class="remover"> 2 </span>');
-  }
-  }).disableSelection();
+	$("#personal").sortable( {
+		placeholder : "ui-state-highlight",
+		change : function(event, ui) {
+			$(".remover").remove();
+			$("ul#personal li a").prepend('<span class="remover"> 2 </span>');
+		}
+	}).disableSelection();
 
-  // Disable sortable on load...
-  $("#personal, #nav ul").sortable("disable");
+	// Disable sortable on load...
+	$("#personal, #nav ul").sortable("disable");
 
-  // Activating remover
-  $('.remover').live('click', function() {
-    $(this).parents("li").remove();
-    return false;
-  });
-  
-  function getMenuEntries() {
-    return ( $("#personal").sortable("toArray") );
-  }
-  
- 
-  
-  $('#saver').live('click', function() {
-	   serialize( getMenuEntries() );
-	  });
+	// Activating remover
+	$('.remover').live('click', function() {
+		$(this).parents("li").remove();
+		return false;
+	});
 
-  $("#normal .main a").click(function() {
-      // One click opens the main menu and and the next click closes the
-      // main menu
-        $('#normal .main').toggleClass('active');
-        // If the main menu is open after toggling...
-        if ($('.main').hasClass('active')) {
-          // enable sortable...
-          $('#personal, #nav ul').sortable('enable');
-          // and add blue border around the personal menu...
-          $('ul#personal').addClass('dotted');
-          $('#personal').after("<a id='saver'>" + saveLabel() + "</a>");
-          // Add the crosses for removing menu entries:
-          $("ul#personal li a").prepend(
-              '<span class="remover"> 2 </span>');
-          $('#remover')
-          // main menu is now closed...
-        } else {
-        	$("#saver").remove();
-          // disable sortable....
-          $('#personal, #nav ul').sortable('disable');
-          // Remove the removers (crosses for removing menu entries):
-          $('.remover').remove();
-          // remove the blue border around the personal menu
-          $('ul#personal').removeClass('dotted');
-          
-          //serialize(getMenuEntries());
-        }
-      })
+	function getMenuEntries() {
+		return ($("#personal").sortable("toArray"));
+	}
+
+	$('#saver').live('click', function() {
+		serialize(getMenuEntries());
+		alert(getSaveMessage());
+	});
+
+	$("#normal .main a").click(function() {
+		// One click opens the main menu and and the next click closes the
+			// main menu
+			$('#normal .main').toggleClass('active');
+			// If the main menu is open after toggling...
+			if ($('.main').hasClass('active')) {
+				// enable sortable...
+				$('#personal, #nav ul').sortable('enable');
+				// and add blue border around the personal menu...
+				$('ul#personal').addClass('dotted');
+				$('#personal').after("<a id='saver'>" + getSaveLabel() + "</a>");
+				// Add the crosses for removing menu entries:
+				$("ul#personal li a").prepend(
+						'<span class="remover"> 2 </span>');
+				$('#remover')
+				// main menu is now closed...
+			} else {
+				$("#saver").remove();
+				// disable sortable....
+				$('#personal, #nav ul').sortable('disable');
+				// Remove the removers (crosses for removing menu entries):
+				$('.remover').remove();
+				// remove the blue border around the personal menu
+				$('ul#personal').removeClass('dotted');
+			}
+		})
 });
