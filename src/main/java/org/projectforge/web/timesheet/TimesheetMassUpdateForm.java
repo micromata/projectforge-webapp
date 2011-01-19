36921019071376faa2cee03bd8f8e38e827cc20a
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-01-19 04:35:28 +0000

    TimesheetFormRenderer.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10107 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/web/timesheet/TimesheetMassUpdateForm.java
+++ src/main/java/org/projectforge/web/timesheet/TimesheetMassUpdateForm.java
@@ -135,13 +135,13 @@ public class TimesheetMassUpdateForm extends AbstractForm<TimesheetDO, Timesheet
     };
     add(kost2Row);
     final LabelValueChoiceRenderer<Integer> kost2ChoiceRenderer = getKost2LabelValueChoiceRenderer();
-    //kost2Choice = TimesheetEditForm.createKost2ChoiceRenderer(timesheetDao, taskTree, kost2ChoiceRenderer, data, kost2List);
+    kost2Choice = TimesheetFormRenderer.createKost2ChoiceRenderer(timesheetDao, taskTree, kost2ChoiceRenderer, data, kost2List);
     kost2Row.add(kost2Choice);
   }
 
   private LabelValueChoiceRenderer<Integer> getKost2LabelValueChoiceRenderer()
   {
-    return null;//TimesheetEditForm.getKost2LabelValueChoiceRenderer(timesheetDao, kost2List, data, kost2Choice);
+    return TimesheetFormRenderer.getKost2LabelValueChoiceRenderer(timesheetDao, kost2List, data, kost2Choice);
   }
 
 }
