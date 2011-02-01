f56022a504190d0834238f7511afabaac52d9d1d
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-02-01 20:25:11 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10233 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/web/wicket/embats/EmbatsUtils.java
@@ -0,0 +1,58 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2011 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.wicket.embats;
+
+import org.projectforge.web.UserAgentBrowser;
+
+/**
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ * 
+ */
+public class EmbatsUtils
+{
+  /**
+   * @param browser
+   * @param version The browser's version.
+   * @return true, if the given browser supports embats, otherwise false.
+   */
+  public static boolean isEmbatsSupported(final UserAgentBrowser browser, final String version)
+  {
+    if (version == null) {
+      return false;
+    }
+    // http://webfonts.info/wiki/index.php?title=%40font-face_browser_support
+    if (browser == UserAgentBrowser.IE) {
+      return version.compareTo("4") >= 0;
+    } else if (browser == UserAgentBrowser.FIREFOX) {
+      return version.compareTo("3.5") >= 0;
+    } else if (browser == UserAgentBrowser.SAFARI) {
+      return version.compareTo("3.1") >= 0;
+    } else if (browser == UserAgentBrowser.CHROME) {
+      return version.compareTo("4") >= 0;
+    } else if (browser == UserAgentBrowser.OPERA) {
+      return version.compareTo("10") >= 0;
+    }
+    return false;
+  }
+}
