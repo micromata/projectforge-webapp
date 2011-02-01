edfd8f451f0f9ed24131c488efb419f3760d46d7
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-02-01 07:32:09 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10228 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/web/wicket/embats/IconLinkPanel.java
@@ -0,0 +1,179 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2011 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.wicket.embats;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.AbstractLink;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.projectforge.web.wicket.AttributeAppendModifier;
+import org.projectforge.web.wicket.MySession;
+import org.projectforge.web.wicket.PresizedImage;
+import org.projectforge.web.wicket.components.TooltipImage;
+
+/**
+ * An image or embats icon as link with an href and with a tooltip.
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ * 
+ */
+public class IconLinkPanel extends Panel
+{
+  private static final long serialVersionUID = -8093620122285662705L;
+
+  public static final String LINK_WICKET_ID = "link";
+
+  public static final String IMAGE_WICKET_ID = "image";
+
+  private boolean embatsSupported;
+
+  private AbstractLink link;
+
+  private IconLinkPanel(final String id)
+  {
+    super(id);
+    final MySession session = (MySession) getSession();
+    embatsSupported = session.isEmbatsSupported();
+  }
+
+  /**
+   * @param embatsChar
+   * @return true if icon was added, false if an image icon is left to add.
+   */
+  private boolean addIcon(final EmbatsChar embatsChar)
+  {
+    if (embatsSupported == true) {
+      link.add(new Label("embatschar", embatsChar.getFontString()).setRenderBodyOnly(true));
+      link.add(new AttributeAppendModifier("class", "embatssymbols"));
+      link.add(new Label(IMAGE_WICKET_ID, "invisible").setVisible(false));
+      return true;
+    } else {
+      link.add(new Label("embatschar", "invisible").setVisible(false));
+      return false;
+    }
+  }
+
+  private void addImage(final EmbatsChar embatsChar, final String tooltip)
+  {
+    if (tooltip != null) {
+      link.add(new PresizedImage(IMAGE_WICKET_ID, getResponse(), embatsChar.getFallbackImage()));
+    } else {
+      link.add(new TooltipImage(IMAGE_WICKET_ID, getResponse(), embatsChar.getFallbackImage(), tooltip));
+    }
+  }
+
+  private void addImage(final EmbatsChar embatsChar, final IModel<String> tooltip)
+  {
+    if (tooltip != null) {
+      link.add(new PresizedImage(IMAGE_WICKET_ID, getResponse(), embatsChar.getFallbackImage()));
+    } else {
+      link.add(new TooltipImage(IMAGE_WICKET_ID, getResponse(), embatsChar.getFallbackImage(), tooltip));
+    }
+  }
+
+  @SuppressWarnings("serial")
+  private void addLink()
+  {
+    link = new Link<Void>("link") {
+      public void onClick()
+      {
+        IconLinkPanel.this.onClick();
+      };
+    };
+    add(link);
+  }
+
+  public IconLinkPanel(final String id, final EmbatsChar embatsChar, final Class< ? > pageClass)
+  {
+    this(id, embatsChar, pageClass, null, (IModel<String>) null);
+  }
+
+  public IconLinkPanel(final String id, final EmbatsChar embatsChar, final Class< ? > pageClass, final PageParameters params)
+  {
+    this(id, embatsChar, pageClass, null, (IModel<String>) null);
+  }
+
+  public IconLinkPanel(final String id, final EmbatsChar embatsChar, final Class< ? > pageClass, final String tooltip)
+  {
+    this(id, embatsChar, pageClass, null, tooltip);
+  }
+
+  @SuppressWarnings("unchecked")
+  public IconLinkPanel(final String id, final EmbatsChar embatsChar, final Class< ? > pageClass, final PageParameters params,
+      final String tooltip)
+  {
+    this(id);
+    if (params != null) {
+      link = new BookmarkablePageLink(LINK_WICKET_ID, pageClass, params);
+    } else {
+      link = new BookmarkablePageLink(LINK_WICKET_ID, pageClass);
+    }
+    add(link);
+    if (addIcon(embatsChar) == false) {
+      addImage(embatsChar, tooltip);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  public IconLinkPanel(final String id, final EmbatsChar embatsChar, final Class< ? > pageClass, final IModel<String> tooltip)
+  {
+    this(id, embatsChar, pageClass, null, tooltip);
+    link = new BookmarkablePageLink(LINK_WICKET_ID, pageClass);
+    add(link);
+    if (addIcon(embatsChar) == false) {
+      addImage(embatsChar, tooltip);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  public IconLinkPanel(final String id, final EmbatsChar embatsChar, final Class< ? > pageClass, final PageParameters params,
+      final IModel<String> tooltip)
+  {
+    this(id);
+    if (params != null) {
+      link = new BookmarkablePageLink(LINK_WICKET_ID, pageClass, params);
+    } else {
+      link = new BookmarkablePageLink(LINK_WICKET_ID, pageClass);
+    }
+    add(link);
+    if (addIcon(embatsChar) == false) {
+      addImage(embatsChar, tooltip);
+    }
+  }
+
+  public IconLinkPanel(final String id, final AbstractLink link, final EmbatsChar embatsChar, final String tooltip)
+  {
+    this(id);
+    addLink();
+    if (addIcon(embatsChar) == false) {
+      addImage(embatsChar, tooltip);
+    }
+  }
+
+  public void onClick()
+  {
+  };
+}
