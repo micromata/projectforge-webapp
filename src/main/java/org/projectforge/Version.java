21b19a1628635f47ba5fe33c56f97c1e1123ec7c
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-02-05 05:40:47 +0000

    New release 3.5.2.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10290 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/Version.java
+++ src/main/java/org/projectforge/Version.java
@@ -17,11 +17,11 @@ public class Version
 
   public static final String APP_TITLE = "ProjectForge 2011";
 
-  public static final String NUMBER = "3.5.1.1";
+  public static final String NUMBER = "3.5.2";
 
-  public static final String RELEASE_DATE = "2011-02-02";
+  public static final String RELEASE_DATE = "2011-02-03";
 
-  public static final String RELEASE_TIMESTAMP = "2011-02-02 01:02";
+  public static final String RELEASE_TIMESTAMP = "2011-02-03 02:21";
 }
 
 		
