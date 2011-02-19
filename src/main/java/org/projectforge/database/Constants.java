5829a5fe7548dcdf14811154001a4e6b862df528
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-02-19 07:53:22 +0000

    PF-197: Plugin technology. Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10424 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/database/Constants.java
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2011 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.database;
+
+
+/**
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ */
+public class Constants
+{
+  /**
+   * Default length of text fields in the data-base (4,000).
+   */
+  public static final int LENGTH_TEXT = 4000;
+  
+  /**
+   * Default length of text fields in the data-base (1,000).
+   */
+  public static final int LENGHT_SUBJECT = 1000;
+
+  /**
+   * Default length of text fields in the data-base (255).
+   */
+  public static final int LENGHT_TITLE = 1000;
+}
