f70bbc4235bf3210f2b9e0b2895e19ba216f416e
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-02-20 22:36:37 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10466 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/admin/UpdateEntryImpl.java
@@ -0,0 +1,71 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2011 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.admin;
+
+import org.projectforge.Version;
+import org.projectforge.common.ReflectionToString;
+
+/**
+ * Represents a update (written in Java).
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ */
+public abstract class UpdateEntryImpl extends UpdateEntry
+{
+  private static final long serialVersionUID = -1178486631632477422L;
+
+  private Version version;
+
+  private String description;
+
+  @Override
+  public Version getVersion()
+  {
+    return version;
+  }
+
+  @Override
+  public void setVersion(final Version version)
+  {
+    this.version = version;
+  }
+
+  @Override
+  public String getDescription()
+  {
+    return this.description;
+  }
+
+  @Override
+  public void setDescription(final String description)
+  {
+    this.description = description;
+  }
+
+  @Override
+  public String toString()
+  {
+    final ReflectionToString tos = new ReflectionToString(this);
+    return tos.toString();
+  }
+}
