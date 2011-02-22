9c84b48cb1a875729e0e719f75675c816d72da9d
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-02-22 01:05:05 +0000

    DATE_FORMATS added.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10487 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/core/ConfigurationParam.java
+++ src/main/java/org/projectforge/core/ConfigurationParam.java
@@ -30,6 +30,7 @@ public enum ConfigurationParam
   SYSTEM_ADMIN_E_MAIL("systemAdministratorEMail", ConfigurationType.STRING), //
   MESSAGE_OF_THE_DAY("messageOfTheDay", ConfigurationType.TEXT), //
   DEFAULT_TIMEZONE("timezone", ConfigurationType.TIME_ZONE, TimeZone.getDefault().getID()), //
+  DATE_FORMATS("dateFormats", ConfigurationType.STRING, "MM/dd/yyyy;dd/MM/yyyy;dd.MM.yyyy;yyyy-MM-dd"), //
   FEEDBACK_E_MAIL("feedbackEMail", ConfigurationType.STRING), //
   FIBU_DEFAULT_VAT("fibu.defaultVAT", ConfigurationType.PERCENT), //
   DEFAULT_TASK_ID_4_ADDRESSES("defaultTask4Addresses", ConfigurationType.TASK), //
