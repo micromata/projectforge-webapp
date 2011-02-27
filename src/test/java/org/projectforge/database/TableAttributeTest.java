b6a182fe1ff71344754124b3d2f3e5770c58ae21
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-02-27 08:50:57 +0000

    Panta rhei.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10574 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/test/java/org/projectforge/database/TableAttributeTest.java
+++ src/test/java/org/projectforge/database/TableAttributeTest.java
@@ -26,25 +26,62 @@ package org.projectforge.database;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
+import org.projectforge.access.GroupTaskAccessDO;
+import org.projectforge.fibu.kost.Kost1DO;
+import org.projectforge.fibu.kost.Kost2ArtDO;
 import org.projectforge.task.TaskDO;
+import org.projectforge.timesheet.TimesheetDO;
+import org.projectforge.user.PFUserDO;
 
 public class TableAttributeTest
 {
   @Test
   public void createTableScript()
   {
-    TableAttribute attr = new TableAttribute(TaskDO.class, "id");
-    assertEquals("column name 'pk' expected.", "pk", attr.getName());
-    assertTrue("primary key", attr.isPrimaryKey());
-    assertFalse("primary key should be not nullable.", attr.isNullable());
-    assertEquals(TableAttributeType.INT, attr.getType());
+    TableAttribute attr;
+    attr = assertAttribute(TaskDO.class, "id", "pk", TableAttributeType.INT, true, false);
+    attr = assertAttribute(TaskDO.class, "created", "created", TableAttributeType.TIMESTAMP, false, true);
+    attr = assertAttribute(TaskDO.class, "lastUpdate", "last_update", TableAttributeType.TIMESTAMP, false, true);
+    attr = assertAttribute(TaskDO.class, "duration", "duration", TableAttributeType.DECIMAL, false, true);
+    assertEquals(2, attr.getScale());
+    assertEquals(10, attr.getPrecision());
+    attr = assertAttribute(TaskDO.class, "title", "title", TableAttributeType.VARCHAR, false, false);
+    assertEquals(TaskDO.TITLE_LENGTH, attr.getLength());
+    attr = assertAttribute(TaskDO.class, "workpackageCode", "workpackage_code", TableAttributeType.VARCHAR, false, true);
+    assertEquals(100, attr.getLength());
+    attr = assertAttribute(TaskDO.class, "description", "description", TableAttributeType.VARCHAR, false, true);
+    assertEquals(TaskDO.DESCRIPTION_LENGTH, attr.getLength());
+    attr = assertAttribute(TaskDO.class, "maxHours", "max_hours", TableAttributeType.INT, false, true);
+    attr = assertAttribute(TaskDO.class, "kost2IsBlackList", "kost2_is_black_list", TableAttributeType.BOOLEAN, false, false);
 
-    attr = new TableAttribute(TaskDO.class, "created");
-    assertEquals("created", attr.getName());
-    assertFalse(attr.isPrimaryKey());
-    assertTrue(attr.isNullable());
-    assertEquals(TableAttributeType.TIMESTAMP, attr.getType());
+    attr = assertAttribute(TimesheetDO.class, "startTime", "start_time", TableAttributeType.TIMESTAMP, false, false);
+    attr = assertAttribute(PFUserDO.class, "loginFailures", "loginFailures", TableAttributeType.INT, false, false);
+    attr = assertAttribute(GroupTaskAccessDO.class, "recursive", "recursive", TableAttributeType.BOOLEAN, false, false);
+    attr = assertAttribute(Kost1DO.class, "nummernkreis", "nummernkreis", TableAttributeType.INT, false, false);
+    try {
+      attr = assertAttribute(Kost2ArtDO.class, "workFraction", "work_fraction", TableAttributeType.DECIMAL, false, true);
+      fail("UnsupportedException expected, because whether scale nor precision is given for this property.");
+    } catch (final UnsupportedOperationException ex) {
+      // Expected, because whether scale nor precision is given.
+    }
+  }
+
+  private TableAttribute assertAttribute(final Class< ? > cls, final String property, final String name, final TableAttributeType type,
+      final boolean primaryKey, final boolean nullable)
+  {
+    final TableAttribute attr = new TableAttribute(cls, property);
+    assertEquals("Different column name expected.", name, attr.getName());
+    assertEquals("Different nullable value expected.", nullable, attr.isNullable());
+    if (primaryKey == true) {
+      assertTrue(attr.isPrimaryKey());
+      assertFalse("Primary key should be not nullable.", attr.isNullable());
+    } else {
+      assertFalse(attr.isPrimaryKey());
+    }
+    assertEquals("Different column type expected.", type, attr.getType());
+    return attr;
   }
 }
