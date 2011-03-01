26986f07b56ca97addec08e1c5ddf1c9e8cdb7a5
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-03-01 16:06:31 +0000

    PF-197: Plugin technology: Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10625 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/plugins/core/PluginsRegistry.java
@@ -0,0 +1,121 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2011 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.settings.IResourceSettings;
+import org.projectforge.admin.SystemUpdater;
+import org.projectforge.core.Configuration;
+import org.projectforge.plugins.todo.ToDoPlugin;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ */
+public class PluginsRegistry
+{
+  private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(PluginsRegistry.class);
+
+  /**
+   * Defines some built-in plugins.
+   */
+  private AbstractPlugin[] builtinPlugins = new AbstractPlugin[] { new ToDoPlugin()};
+
+
+  private static PluginsRegistry instance;
+
+  private List<AbstractPlugin> plugins = new ArrayList<AbstractPlugin>();
+
+  public synchronized static PluginsRegistry instance()
+  {
+    if (instance == null) {
+      instance = new PluginsRegistry();
+    }
+    return instance;
+  }
+
+  public void register(final AbstractPlugin plugin)
+  {
+    plugins.add(plugin);
+  }
+
+  public List<AbstractPlugin> getPlugins()
+  {
+    return plugins;
+  }
+
+  public PluginsRegistry()
+  {
+    for (final AbstractPlugin plugin : builtinPlugins) {
+      plugins.add(plugin);
+    }
+    final Configuration configuration = Configuration.getInstance();
+    final String[] pluginMainClasses = configuration.getPluginMainClasses();
+    if (pluginMainClasses != null) {
+      for (final String pluginMainClassName : pluginMainClasses) {
+        try {
+          final Class< ? > pluginMainClass = Class.forName(pluginMainClassName);
+          try {
+            final AbstractPlugin plugin = (AbstractPlugin) pluginMainClass.newInstance();
+            plugins.add(plugin);
+          } catch (final ClassCastException ex) {
+            log.error("Couldn't load plugin, class '" + pluginMainClassName + "' isn't of type AbstractPlugin.");
+          } catch (final InstantiationException ex) {
+            log.error("Couldn't load plugin, class '" + pluginMainClassName + "' can't be instantiated: " + ex);
+          } catch (final IllegalAccessException ex) {
+            log.error("Couldn't load plugin, class '" + pluginMainClassName + "' can't be instantiated: " + ex);
+          }
+        } catch (final ClassNotFoundException ex) {
+          log.error("Couldn't load plugin, class '" + pluginMainClassName + "' not found");
+        }
+      }
+    }
+  }
+
+  public void set(final SystemUpdater systemUpdater)
+  {
+    for (final AbstractPlugin plugin : plugins) {
+      systemUpdater.register(plugin.getInitializationUpdateEntry());
+      systemUpdater.register(plugin.getUpdateEntries());
+    }
+  }
+
+  public void set(final ConfigurableListableBeanFactory beanFactory, final IResourceSettings resourceSettings)
+  {
+    for (final AbstractPlugin plugin : plugins) {
+      plugin.setResourceSettings(resourceSettings);
+      beanFactory.autowireBeanProperties(plugin, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
+    }
+  }
+
+  public void initialize()
+  {
+    for (final AbstractPlugin plugin : plugins) {
+      plugin.init();
+    }
+  }
+}
