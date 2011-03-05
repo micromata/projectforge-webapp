e66404c6d85fb12a459eba5289d0167b053aadbd
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-03-05 17:39:29 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10699 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/test/java/org/projectforge/plugins/memo/MemoTableTest.java
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2011 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.memo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.projectforge.database.Table;
+import org.projectforge.database.TableAttribute;
+import org.projectforge.task.TaskDO;
+import org.projectforge.timesheet.TimesheetDO;
+import org.projectforge.user.PFUserDO;
+
+public class MemoTableTest
+{
+  @Test
+  public void createTables()
+  {
+    assertEquals("T_PLUGIN_MEMO", new Table(MemoDO.class).getName());
+
+    final Table table = new Table(MemoDO.class);
+    assertEquals("T_PLUGIN_MEMO", table.getName());
+    table.addAttributes("id");
+    final TableAttribute attr = table.getAttributes().get(0);
+    assertEquals("id", attr.getProperty());
+    assertEquals("pk", attr.getName());
+  }
+}
