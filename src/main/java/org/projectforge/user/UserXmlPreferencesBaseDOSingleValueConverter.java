b68741ac54b46934f000ce2a94d3ae3add9e339a
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-03-10 04:25:55 +0000

    PF-228: Use id for marshaling and unmarshaling data base objects. Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10755 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/user/UserXmlPreferencesBaseDOSingleValueConverter.java
@@ -0,0 +1,88 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2011 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.user;
+
+import org.apache.commons.lang.StringUtils;
+import org.projectforge.core.BaseDO;
+import org.projectforge.core.BaseDao;
+import org.projectforge.registry.Registry;
+
+import com.thoughtworks.xstream.converters.SingleValueConverter;
+
+/**
+ * Converts BaseDO from and to strings (using the id).
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ */
+public class UserXmlPreferencesBaseDOSingleValueConverter implements SingleValueConverter
+{
+  private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(UserXmlPreferencesBaseDOSingleValueConverter.class);
+  
+  private Class< ? extends BaseDao< ? >> daoClass ;
+  
+  private Class<? extends BaseDO<?>> doClass;
+  
+  /**
+   * Marshals only the id and unmarshals by loading the instance by id from the dao.
+   * @param daoClass Class of the dao.
+   * @param doClass Class of the DO which will be converted.
+   * @see BaseDao#getOrLoad(Integer)
+   */
+  public UserXmlPreferencesBaseDOSingleValueConverter(final Class< ? extends BaseDao< ? >> daoClass, final Class<? extends BaseDO<?>> doClass)
+  {
+    this.daoClass = daoClass;
+    this.doClass = doClass;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public boolean canConvert(final Class type)
+  {
+    return doClass.isAssignableFrom(type);
+  }
+
+  @Override
+  public String toString(final Object obj)
+  {
+    if (obj == null) {
+      return null;
+    }
+    return String.valueOf(((BaseDO< ? >) obj).getId());
+  }
+
+  @Override
+  public Object fromString(final String str)
+  {
+    if (StringUtils.isBlank(str) == true) {
+      return null;
+    }
+    final Integer id = Integer.parseInt(str);
+    final BaseDao<?> dao = Registry.instance().getDao(daoClass);
+    if (dao == null) {
+      log.error("Could not get dao '" + daoClass + "'. It's not registerd in the Registry.");
+      return null;
+    } else {
+      return dao.getOrLoad(id);
+    }
+  }
+}
