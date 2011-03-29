1a6cf9e4787835fd45de5e6e8b17131c35cff393
cclaus <cclaus@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-03-29 13:21:51 +0000

    Added basic functionality to create, edit and delete GWiki-Spaces
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10909 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/web/core/SpaceEditPage.java
+++ src/main/java/org/projectforge/web/core/SpaceEditPage.java
@@ -23,6 +23,9 @@
 
 package org.projectforge.web.core;
 
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
@@ -30,8 +33,20 @@ import org.projectforge.core.SpaceDO;
 import org.projectforge.core.SpaceDao;
 import org.projectforge.core.SpaceStatus;
 import org.projectforge.web.wicket.AbstractAutoLayoutEditPage;
+import org.projectforge.web.wicket.AbstractBasePage;
 import org.projectforge.web.wicket.AbstractEditPage;
 import org.projectforge.web.wicket.EditPage;
+
+import de.micromata.genome.gwiki.model.GWikiArtefakt;
+import de.micromata.genome.gwiki.model.GWikiElement;
+import de.micromata.genome.gwiki.model.GWikiElementInfo;
+import de.micromata.genome.gwiki.model.GWikiPropKeys;
+import de.micromata.genome.gwiki.model.GWikiProps;
+import de.micromata.genome.gwiki.model.GWikiSettingsProps;
+import de.micromata.genome.gwiki.model.GWikiWebUtils;
+import de.micromata.genome.gwiki.page.GWikiContext;
+import de.micromata.genome.gwiki.page.GWikiStandaloneContext;
+import de.micromata.genome.gwiki.page.impl.GWikiWikiPageBaseArtefakt;
 
 @EditPage(defaultReturnPage = SpaceListPage.class)
 public class SpaceEditPage extends AbstractAutoLayoutEditPage<SpaceDO, SpaceEditForm, SpaceDao>
@@ -43,7 +58,7 @@ public class SpaceEditPage extends AbstractAutoLayoutEditPage<SpaceDO, SpaceEdit
   @SpringBean(name = "spaceDao")
   private SpaceDao spaceDao;
 
-  public SpaceEditPage(PageParameters parameters)
+  public SpaceEditPage(final PageParameters parameters)
   {
     super(parameters, "space");
     init();
@@ -59,9 +74,154 @@ public class SpaceEditPage extends AbstractAutoLayoutEditPage<SpaceDO, SpaceEdit
   }
 
   @Override
-  protected SpaceEditForm newEditForm(AbstractEditPage< ? , ? , ? > parentPage, SpaceDO data)
+  protected SpaceEditForm newEditForm(final AbstractEditPage< ? , ? , ? > parentPage, final SpaceDO data)
   {
     return new SpaceEditForm(this, data);
+  }
+
+  /**
+   * Removes a GWiki-Space recursivley
+   * 
+   * @see org.projectforge.web.wicket.AbstractEditPage#onDelete()
+   */
+  @Override
+  public AbstractBasePage onDelete()
+  {
+    final GWikiStandaloneContext wikiContext = GWikiStandaloneContext.create();
+    final String pageId = GWikiContext.getPageIdFromTitle(getForm().getData().getIdentifier()) + "/Index";
+
+    final GWikiElement element = wikiContext.getWikiWeb().findElement(pageId);
+
+    if (element != null) {
+      removeElementChildren(wikiContext, element.getElementInfo());
+    }
+    
+    // TODO (cclaus) TextExtracts and TextIndex Fragments are still present after deleting!
+
+    return super.onDelete();
+  }
+
+  /**
+   * Recursive function to remove all element children, before the element itself will be removed.
+   * 
+   * @param wikiContext
+   * @param elementInfo
+   */
+  private void removeElementChildren(final GWikiContext wikiContext, final GWikiElementInfo elementInfo)
+  {
+    /*
+     * TODO (cclaus) check rights necessary?
+     * 
+     * if (wikiContext.getWikiWeb().getAuthorization().isAllowToEdit(wikiContext, elementInfo) == false) { return; }
+     */
+    final List<GWikiElementInfo> cl = wikiContext.getElementFinder().getAllDirectChilds(elementInfo);
+    for (final GWikiElementInfo childElementInfo : cl) {
+      removeElementChildren(wikiContext, childElementInfo);
+    }
+
+    final GWikiElement element = wikiContext.getWikiWeb().findElement(elementInfo.getId());
+
+    wikiContext.getWikiWeb().removeWikiPage(wikiContext, element);
+  }
+
+  /**
+   * Creates a gwiki page: /gwiki/<SPACENAME>/Index if the space doesn't exist Otherwise the corresponding space will be updated by the PF
+   * configuration dialog.
+   * 
+   * @see org.projectforge.web.wicket.AbstractEditPage#onSaveOrUpdate()
+   */
+  @Override
+  public AbstractBasePage onSaveOrUpdate()
+  {
+    final SpaceDO spaceData = getForm().getData();
+    final GWikiStandaloneContext wikiContext = GWikiStandaloneContext.create();
+    final String metaTemplateId = "admin/templates/StandardWikiPageMetaTemplate";
+    final String pageId = GWikiContext.getPageIdFromTitle(spaceData.getIdentifier()) + "/Index";
+    final String pageIntro = "{pageintro}";
+
+    // TODO: (cclaus) check rights necessary?
+
+    /*
+     * Test, if pageId already exists. If not, a new space will be created
+     */
+    if (wikiContext.getWikiWeb().findElement(pageId) == null) {
+      // Creates GWiki Properties
+      final GWikiProps props = new GWikiSettingsProps();
+      props.setStringValue(GWikiPropKeys.WIKIMETATEMPLATE, metaTemplateId);
+      props.setStringValue(GWikiPropKeys.TYPE, metaTemplateId);
+      props.setStringValue(GWikiPropKeys.PARENTPAGE, "Index");
+      props.setStringValue(GWikiPropKeys.TITLE, spaceData.getTitle());
+
+      // Creates new GWiki Element
+      final GWikiElement element = GWikiWebUtils.createNewElement(wikiContext, pageId, metaTemplateId, spaceData.getTitle());
+      element.getElementInfo().setProps(props);
+
+      // Gets GWiki 'MainPage'. All wiki content will be stored in this artifact
+      final GWikiArtefakt< ? > part = element.getPart("MainPage");
+
+      // Tests if the 'MainPage' is a GWikiWikiPage
+      if (part != null && part instanceof GWikiWikiPageBaseArtefakt) {
+        final GWikiWikiPageBaseArtefakt art = (GWikiWikiPageBaseArtefakt) part;
+        final StringBuilder content = new StringBuilder();
+
+        // Appends {pageIntro} description {pageIntro}
+        if (StringUtils.isNotEmpty(spaceData.getDescription())) {
+          content.append(pageIntro).append(spaceData.getDescription());
+          content.append(pageIntro).append("\n\n");
+        }
+
+        // Appends children macro
+        content.append("{children:depth=1}");
+
+        // Sets storage data to the GWiki artifact
+        art.setStorageData(content.toString());
+        art.compileFragements(wikiContext);
+      }
+
+      // saves Element and updates page caches
+      wikiContext.getWikiWeb().saveElement(wikiContext, element, false);
+    }
+
+    /*
+     * In update case: updates element information and description content
+     */
+    else {
+      final GWikiElement element = wikiContext.getWikiWeb().findElement(pageId);
+
+      final GWikiProps props = element.getElementInfo().getProps();
+      props.setStringValue(GWikiPropKeys.PAGEID, spaceData.getIdentifier());
+      props.setStringValue(GWikiPropKeys.TITLE, spaceData.getTitle());
+
+      final GWikiArtefakt< ? > part = element.getPart("MainPage");
+
+      // Gets the Wiki code and updates it's description when needed
+      if (part != null && part instanceof GWikiWikiPageBaseArtefakt) {
+        final GWikiWikiPageBaseArtefakt art = (GWikiWikiPageBaseArtefakt) part;
+
+        final StringBuilder builder = new StringBuilder(art.getStorageData());
+        final int startIndex = builder.indexOf(pageIntro) + pageIntro.length();
+        final int endIndex = builder.indexOf(pageIntro, startIndex);
+
+        if (StringUtils.isNotEmpty(spaceData.getDescription())) {
+          if (startIndex != 10) {
+            builder.delete(startIndex, endIndex);
+            builder.insert(startIndex, spaceData.getDescription());
+          } else {
+            builder.insert(0, pageIntro + spaceData.getDescription() + pageIntro);
+          }
+
+        } else {
+          builder.delete(0, endIndex + pageIntro.length());
+        }
+
+        art.setStorageData(builder.toString());
+        art.compileFragements(wikiContext);
+      }
+
+      wikiContext.getWikiWeb().saveElement(wikiContext, element, false);
+    }
+
+    return super.onSaveOrUpdate();
   }
 
   @Override
@@ -69,4 +229,5 @@ public class SpaceEditPage extends AbstractAutoLayoutEditPage<SpaceDO, SpaceEdit
   {
     return log;
   }
+
 }
