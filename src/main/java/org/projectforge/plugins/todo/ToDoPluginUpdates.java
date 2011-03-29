be68978f5fb4a1b76beebc9a3ff4cf0634047552
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-03-29 05:38:43 +0000

    Table doesn't work as member var of UpdateEntryImpl in some error cases (attributes are added twice).
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@10902 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/plugins/todo/ToDoPluginUpdates.java
+++ src/main/java/org/projectforge/plugins/todo/ToDoPluginUpdates.java
@@ -43,11 +43,10 @@ public class ToDoPluginUpdates
   public static UpdateEntry getInitializationUpdateEntry()
   {
     return new UpdateEntryImpl(ToDoPlugin.ID, "1.0.0", "2011-03-08", "Adds table T_PLUGIN_TODO.") {
-      final Table table = new Table(ToDoDO.class);
-
       @Override
       public UpdatePreCheckStatus runPreCheck()
       {
+        final Table table = new Table(ToDoDO.class);
         // Does the data-base table already exist?
         return dao.doesExist(table) == true ? UpdatePreCheckStatus.ALREADY_UPDATED : UpdatePreCheckStatus.OK;
       }
@@ -57,8 +56,8 @@ public class ToDoPluginUpdates
       {
         // Create initial data-base table:
         final Table table = new Table(ToDoDO.class) //
-            .addAttributes("id", "created", "lastUpdate", "deleted", "reporter", "assignee", "task", "group", "subject", "comment",
-                "description", "status", "recent", "type", "priority", "dueDate", "resubmission");
+        .addAttributes("id", "created", "lastUpdate", "deleted", "reporter", "assignee", "task", "group", "subject", "comment",
+            "description", "status", "recent", "type", "priority", "dueDate", "resubmission");
         dao.createTable(table);
         dao.createMissingIndices();
         return UpdateRunningStatus.DONE;
