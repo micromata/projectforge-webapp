e7b8a48ceb47e847e3d6407f0836a363d686f143
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-05-28 21:51:10 +0000

    Panta rhei.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@11061 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/test/java/org/projectforge/web/address/AddressPagesTest.java
+++ src/test/java/org/projectforge/web/address/AddressPagesTest.java
@@ -25,24 +25,48 @@ package org.projectforge.web.address;
 
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.util.tester.FormTester;
 import org.junit.Assert;
 import org.junit.Test;
 import org.projectforge.address.AddressDO;
+import org.projectforge.address.AddressStatus;
+import org.projectforge.address.ContactStatus;
+import org.projectforge.address.FormOfAddress;
 import org.projectforge.web.wicket.WicketPageTestBase;
 
 public class AddressPagesTest extends WicketPageTestBase
 {
   @SuppressWarnings("unchecked")
   @Test
-  public void testRenderMyPage()
+  public void testRenderPages()
   {
     loginTestAdmin();
     tester.startPage(new AddressListPage(new PageParameters()));
     tester.assertRenderedPage(AddressListPage.class);
-    final DataTable<AddressDO> table = (DataTable<AddressDO>)tester.getComponentFromLastRenderedPage("body:form:table");
+    DataTable<AddressDO> table = (DataTable<AddressDO>) tester.getComponentFromLastRenderedPage("body:form:table");
     Assert.assertEquals(0, table.getRowCount());
     // Now, add a new address:
     tester.clickLink("body:contentMenuArea:menu:1:link");
     tester.assertRenderedPage(AddressEditPage.class);
+    // Need new page to initialize model:
+    AddressEditPage editPage = new AddressEditPage(new PageParameters());
+    final AddressDO data = editPage.getForm().getData();
+    data.setName("Reinhard").setFirstName("Kai").setForm(FormOfAddress.MISTER).setContactStatus(ContactStatus.ACTIVE).setAddressStatus(
+        AddressStatus.UPTODATE).setTask(getTask("1.1"));
+    tester.startPage(editPage);
+    FormTester form = tester.newFormTester("body:form");
+    form.submit("create:button");
+    tester.assertRenderedPage(AddressListPage.class);
+    table = (DataTable<AddressDO>) tester.getComponentFromLastRenderedPage("body:form:table");
+    Assert.assertEquals(1, table.getRowCount());
+    tester.clickLink("body:form:table:body:rows:1:cells:1:cell:1:select"); // Edit page
+    tester.assertRenderedPage(AddressEditPage.class);
+    editPage = (AddressEditPage) tester.getLastRenderedPage();
+    Assert.assertEquals("Kai", editPage.getForm().getData().getFirstName());
+    form = tester.newFormTester("body:form");
+    form.submit("cancel:button");
+    tester.assertRenderedPage(AddressListPage.class);
+    tester.clickLink("body:form:table:body:rows:2:cells:1:cell:2:link"); // View page
+    tester.assertRenderedPage(AddressViewPage.class);
   }
 }
