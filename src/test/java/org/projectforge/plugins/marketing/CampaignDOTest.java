0f61e48abb7601c766574fbc424ccded5e498f42
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2011-11-24 16:45:17 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@11097 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/test/java/org/projectforge/plugins/marketing/CampaignDOTest.java
@@ -0,0 +1,51 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2011 Kai Reinhard (k.reinhard@me.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.marketing;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class CampaignDOTest
+{
+  @Test
+  public void getValuesAsArray()
+  {
+    assertNull(getValues(null));
+    assertNull(getValues(""));
+    assertNull(getValues("   "));
+    assertArrayEquals(new String[] { "true"}, getValues("true; "));
+    assertArrayEquals(new String[] { "true", "false"}, getValues("true; false"));
+  }
+
+  private String[] getValues(final String values)
+  {
+    final CampaignDO campaign = new CampaignDO();
+    campaign.setValues(values);
+    final String[] result = campaign.getValuesArray();
+    assertArrayEquals(result, CampaignDO.getValuesArray(campaign.getValues()));
+    return result;
+  }
+}
