91e5c19f0a7f22bf8028ade7d0378abfa952c2f8
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-01-02 09:18:23 +0000

    Refactoring started (not yet finished): Select customer as autocomplete text field.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@11179 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/web/fibu/CustomerSelectPanel.java
+++ src/main/java/org/projectforge/web/fibu/CustomerSelectPanel.java
@@ -23,21 +23,28 @@
 
 package org.projectforge.web.fibu;
 
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.SubmitLink;
-import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.util.convert.IConverter;
 import org.projectforge.common.NumberHelper;
+import org.projectforge.common.RecentQueue;
+import org.projectforge.core.BaseSearchFilter;
 import org.projectforge.fibu.KundeDO;
+import org.projectforge.fibu.KundeDao;
 import org.projectforge.fibu.KundeFavorite;
 import org.projectforge.fibu.KundeFormatter;
-import org.projectforge.user.UserPrefArea;
 import org.projectforge.web.wicket.AbstractForm;
 import org.projectforge.web.wicket.AbstractSelectPanel;
 import org.projectforge.web.wicket.WebConstants;
+import org.projectforge.web.wicket.autocompletion.PFAutoCompleteTextField;
 import org.projectforge.web.wicket.components.FavoritesChoicePanel;
 import org.projectforge.web.wicket.components.MaxLengthTextField;
 import org.projectforge.web.wicket.components.TooltipImage;
@@ -55,30 +62,136 @@ public class CustomerSelectPanel extends AbstractSelectPanel<KundeDO>
   @SpringBean(name = "kundeFormatter")
   private KundeFormatter kundeFormatter;
 
-  private PropertyModel<String> kundeText;
+  private final PropertyModel<String> kundeText;
 
-  private TextField<String> kundeTextField;
+  private RecentQueue<String> recentCustomers;
+
+  private PFAutoCompleteTextField<KundeDO> customerTextField;
+
+  // Only used for detecting changes:
+  private KundeDO currentCustomer;
+
+  @SpringBean(name = "customerDao")
+  private KundeDao customerDao;
 
   /**
    * @param id
    * @param model
-   * @param kundeText If no Kunde is given then a free text field representing a Kunde can be used.
+   * @param customerText If no Kunde is given then a free text field representing a Kunde can be used.
    * @param caller
    * @param selectProperty
    */
-  public CustomerSelectPanel(final String id, final IModel<KundeDO> model, final PropertyModel<String> kundeText,
+  public CustomerSelectPanel(final String id, final IModel<KundeDO> model, final PropertyModel<String> customerText,
       final ISelectCallerPage caller, final String selectProperty)
   {
     super(id, model, caller, selectProperty);
-    this.kundeText = kundeText;
+    this.customerText = customerText;
   }
 
+  @Override
   @SuppressWarnings("serial")
   public CustomerSelectPanel init()
   {
     super.init();
-    if (kundeText != null) {
-      kundeTextField = new MaxLengthTextField("kundeText", kundeText) {
+    customerTextField = new PFAutoCompleteTextField<KundeDO>("customerField", getModel()) {
+      @Override
+      protected List<KundeDO> getChoices(final String input)
+      {
+        final BaseSearchFilter filter = new BaseSearchFilter();
+        filter.setSearchFields("name", "identifier");
+        filter.setSearchString(input);
+        final List<KundeDO> list = customerDao.getList(filter);
+        return list;
+      }
+
+      @Override
+      protected List<String> getRecentUserInputs()
+      {
+        return getRecentUsers().getRecents();
+      }
+
+      @Override
+      protected String formatLabel(final KundeDO customer)
+      {
+        if (customer == null) {
+          return "";
+        }
+        return formatKunde(customer);
+      }
+
+      @Override
+      protected String formatValue(final KundeDO customer)
+      {
+        if (customer == null) {
+          return "";
+        }
+        return customer.getName();
+      }
+      @Override
+      protected void convertInput()
+      {
+        final KundeDO customer = (KundeDO) getConverter(getType()).convertToObject(getInput(), getLocale());
+        setConvertedInput(customer);
+        if (customer != null && (currentCustomer == null || customer.getId() != currentCustomer.getId())) {
+          getRecentKundes().append(formatKunde(customer));
+        }
+        currentCustomer = customer;
+      }
+
+      @Override
+      public IConverter getConverter(final Class< ? > type)
+      {
+        return new IConverter() {
+          @Override
+          public Object convertToObject(final String value, final Locale locale)
+          {
+            if (StringUtils.isEmpty(value) == true) {
+              getModel().setObject(null);
+              return null;
+            }
+            final int ind = value.indexOf(": ");
+            final String customername = ind >= 0 ? value.substring(0, ind) : value;
+            final KundeDO customer = customerDao.getKundeGroupCache().getKunde(customername);
+            if (customer == null) {
+              error(getString("customer.panel.error.customernameNotFound"));
+            }
+            getModel().setObject(customer);
+            return customer;
+          }
+
+          @Override
+          public String convertToString(final Object value, final Locale locale)
+          {
+            if (value == null) {
+              return "";
+            }
+            final KundeDO customer = (KundeDO) value;
+            return customer.getKundename();
+          }
+        };
+      }
+    };
+    currentKunde = getModelObject();
+    customerTextField.enableTooltips().withLabelValue(true).withMatchContains(true).withMinChars(2).withAutoSubmit(false).withWidth(400);
+    customerTextField.setLabel(new Model<String>() {
+      @Override
+      public String getObject()
+      {
+        if (label != null) {
+          return label;
+        } else {
+          return getString("customer");
+        }
+      }
+    });
+
+
+
+
+
+
+    if (customerText != null) {
+      customerTextField = new MaxLengthTextField("customerText", customerText) {
         @Override
         public boolean isVisible()
         {
@@ -86,21 +199,22 @@ public class CustomerSelectPanel extends AbstractSelectPanel<KundeDO>
               .greaterZero(CustomerSelectPanel.this.getModelObject().getId()) == false);
         }
       };
-      add(kundeTextField);
+      add(customerTextField);
     } else {
-      add(AbstractForm.createInvisibleDummyComponent("kundeText"));
+      add(AbstractForm.createInvisibleDummyComponent("customerText"));
     }
-    final Label kundeAsStringLabel = new Label("kundeAsString", new Model<String>() {
+    final Label customerAsStringLabel = new Label("customerAsString", new Model<String>() {
 
       @Override
       public String getObject()
       {
-        final KundeDO kunde = getModelObject();
-        return kundeFormatter.format(kunde, false);
+        final KundeDO customer = getModelObject();
+        return customerFormatter.format(customer, false);
       }
     });
-    add(kundeAsStringLabel);
+    add(customerAsStringLabel);
     final SubmitLink selectButton = new SubmitLink("select") {
+      @Override
       public void onSubmit()
       {
         setResponsePage(new CustomerListPage(caller, selectProperty));
@@ -128,7 +242,7 @@ public class CustomerSelectPanel extends AbstractSelectPanel<KundeDO>
         getString("fibu.tooltip.unselectKunde")));
     // DropDownChoice favorites
     final FavoritesChoicePanel<KundeDO, KundeFavorite> favoritesPanel = new FavoritesChoicePanel<KundeDO, KundeFavorite>("favorites",
-        UserPrefArea.KUNDE_FAVORITE, tabIndex, "half select") {
+        KundePrefArea.KUNDE_FAVORITE, tabIndex, "half select") {
       @Override
       protected void select(final KundeFavorite favorite)
       {
@@ -160,22 +274,22 @@ public class CustomerSelectPanel extends AbstractSelectPanel<KundeDO>
   }
 
   /**
-   * Will be called if the user has chosen an entry of the kunde favorites drop down choice.
-   * @param kunde
+   * Will be called if the customer has chosen an entry of the customer favorites drop down choice.
+   * @param customer
    */
-  protected void selectKunde(final KundeDO kunde)
+  protected void selectKunde(final KundeDO customer)
   {
-    setModelObject(kunde);
-    caller.select(selectProperty, kunde.getId());
+    setModelObject(customer);
+    caller.select(selectProperty, customer.getId());
   }
 
   /**
-   * @return The user's raw input of kunde text if given, otherwise null.
+   * @return The customer's raw input of customer text if given, otherwise null.
    */
   public String getKundeTextInput()
   {
-    if (kundeTextField != null) {
-      return kundeTextField.getRawInput();
+    if (customerTextField != null) {
+      return customerTextField.getRawInput();
     }
     return null;
   }
