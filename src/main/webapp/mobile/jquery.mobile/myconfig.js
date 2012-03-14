4ff481b75af4ca2f9043cfd7fcc896773c666745
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-03-14 16:02:19 +0000

    jquery-mobile-1.0.1.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@11393 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/webapp/mobile/jquery.mobile/myconfig.js
+++ src/main/webapp/mobile/jquery.mobile/myconfig.js
@@ -1,4 +1,4 @@
-$(document).bind("mobileinit", function(){
- /*	To much problems with path errors. Waiting until final release. */
-  $.mobile.ajaxEnabled = false;
-});
+//$(document).bind("mobileinit", function(){
+// /*	To much problems with path errors. Waiting until final release. */
+//  $.mobile.ajaxEnabled = false;
+//});
