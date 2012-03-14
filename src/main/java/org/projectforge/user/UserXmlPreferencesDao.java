ec6dc53ddb9a37f37bea9e893895813dec36477d
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-03-14 15:53:04 +0000

    New xstream version (classloader will no be set).
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@11369 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/user/UserXmlPreferencesDao.java
+++ src/main/java/org/projectforge/user/UserXmlPreferencesDao.java
@@ -58,11 +58,12 @@ public class UserXmlPreferencesDao extends HibernateDaoSupport
 
   private AccessChecker accessChecker;
 
-  private XStream xstream;
+  private final XStream xstream;
 
   public UserXmlPreferencesDao()
   {
     xstream = new XStream();
+    xstream.setClassLoader(getClass().getClassLoader()); // Crashes without setClassLoader() if started via StartHelper.
     xstream.processAnnotations(new Class< ? >[] { UserXmlPreferencesMap.class, TaskFilter.class, TimesheetPrefData.class,
         ScriptCallData.class, RecentScriptCalls.class});
     registerConverter(UserDao.class, PFUserDO.class, 20);
@@ -92,7 +93,7 @@ public class UserXmlPreferencesDao extends HibernateDaoSupport
     xstream.registerConverter(new UserXmlPreferencesBaseDOSingleValueConverter(daoClass, doClass), priority);
   }
 
-  public void setAccessChecker(AccessChecker accessChecker)
+  public void setAccessChecker(final AccessChecker accessChecker)
   {
     this.accessChecker = accessChecker;
   }
@@ -102,7 +103,7 @@ public class UserXmlPreferencesDao extends HibernateDaoSupport
    * the id of the context user.
    * @param userId
    */
-  private UserXmlPreferencesDO getUserPreferencesByUserId(Integer userId, String key, boolean checkAccess)
+  private UserXmlPreferencesDO getUserPreferencesByUserId(final Integer userId, final String key, final boolean checkAccess)
   {
     if (checkAccess == true) {
       checkAccess(userId);
@@ -122,7 +123,7 @@ public class UserXmlPreferencesDao extends HibernateDaoSupport
    * @param userId
    */
   @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
-  public List<UserXmlPreferencesDO> getUserPreferencesByUserId(Integer userId)
+  public List<UserXmlPreferencesDO> getUserPreferencesByUserId(final Integer userId)
   {
     checkAccess(userId);
     @SuppressWarnings("unchecked")
@@ -148,13 +149,13 @@ public class UserXmlPreferencesDao extends HibernateDaoSupport
    * @param userPrefs
    * @param logError
    */
-  protected Object deserialize(UserXmlPreferencesDO userPrefs, boolean logError)
+  protected Object deserialize(final UserXmlPreferencesDO userPrefs, final boolean logError)
   {
     String xml = null;
     try {
       UserXmlPreferencesMigrationDao.migrate(userPrefs);
       xml = userPrefs.getSerializedSettings();
-      final Object value = (Object) xstream.fromXML(xml);
+      final Object value = xstream.fromXML(xml);
       return value;
     } catch (final Throwable ex) {
       if (logError == true) {
@@ -189,16 +190,16 @@ public class UserXmlPreferencesDao extends HibernateDaoSupport
   }
 
   @Transactional(readOnly = false, propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ)
-  public void saveOrUpdate(Integer userId, String key, Object entry, boolean checkAccess)
+  public void saveOrUpdate(final Integer userId, final String key, final Object entry, final boolean checkAccess)
   {
     if (accessChecker.isDemoUser(userId) == true) {
       // Do nothing.
       return;
     }
-    String xml = xstream.toXML(entry);
+    final String xml = xstream.toXML(entry);
     boolean isNew = false;
     UserXmlPreferencesDO userPrefs = getUserPreferencesByUserId(userId, key, checkAccess);
-    Date date = new Date();
+    final Date date = new Date();
     if (userPrefs == null) {
       isNew = true;
       userPrefs = new UserXmlPreferencesDO();
@@ -223,7 +224,7 @@ public class UserXmlPreferencesDao extends HibernateDaoSupport
   }
 
   @Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW, isolation = Isolation.REPEATABLE_READ)
-  public void remove(Integer userId, String key)
+  public void remove(final Integer userId, final String key)
   {
     if (accessChecker.isDemoUser(userId) == true) {
       // Do nothing.
