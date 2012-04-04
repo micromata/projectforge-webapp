f5249d3b54e8e65cc317fca8df565b6e08511980
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-04-04 07:04:03 +0000

    Panta rhei.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@11662 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/webapp/scripts/jquery.jstree/jquery.jstree.js
+++ src/main/webapp/scripts/jquery.jstree/jquery.jstree.js
@@ -3727,36 +3727,36 @@
 					"separator_after"	: false,
 					"label"				: "Delete",
 					"action"			: function (obj) { if(this.is_selected(obj)) { this.remove(); } else { this.remove(obj); } }
-				},
-				"ccp" : {
-					"separator_before"	: true,
-					"icon"				: false,
-					"separator_after"	: false,
-					"label"				: "Edit",
-					"action"			: false,
-					"submenu" : { 
-						"cut" : {
-							"separator_before"	: false,
-							"separator_after"	: false,
-							"label"				: "Cut",
-							"action"			: function (obj) { this.cut(obj); }
-						},
-						"copy" : {
-							"separator_before"	: false,
-							"icon"				: false,
-							"separator_after"	: false,
-							"label"				: "Copy",
-							"action"			: function (obj) { this.copy(obj); }
-						},
-						"paste" : {
-							"separator_before"	: false,
-							"icon"				: false,
-							"separator_after"	: false,
-							"label"				: "Paste",
-							"action"			: function (obj) { this.paste(obj); }
-						}
-					}
-				}
+				}//, Kai Reinhard: removed
+// "ccp" : {
+// "separator_before" : true,
+// "icon" : false,
+// "separator_after" : false,
+// "label" : "Edit",
+// "action" : false,
+// "submenu" : {
+// "cut" : {
+// "separator_before" : false,
+// "separator_after" : false,
+// "label" : "Cut",
+// "action" : function (obj) { this.cut(obj); }
+// },
+// "copy" : {
+// "separator_before" : false,
+// "icon" : false,
+// "separator_after" : false,
+// "label" : "Copy",
+// "action" : function (obj) { this.copy(obj); }
+// },
+// "paste" : {
+// "separator_before" : false,
+// "icon" : false,
+// "separator_after" : false,
+// "label" : "Paste",
+// "action" : function (obj) { this.paste(obj); }
+// }
+// }
+// }
 			}
 		},
 		_fn : {
