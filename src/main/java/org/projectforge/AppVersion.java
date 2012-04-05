b8abbec7e1abd802ea3fae60be80a933e0b1253c
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-04-05 15:39:32 +0000

    Beta 1.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@11696 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/AppVersion.java
+++ src/main/java/org/projectforge/AppVersion.java
@@ -17,13 +17,13 @@ public class AppVersion
 
   public static final String APP_TITLE = "ProjectForge 2012";
 
-  public static final Version VERSION = new Version("4.0.0");
+  public static final Version VERSION = new Version("4.0.0b1");
 
   public static final String NUMBER = VERSION.toString();
 
-  public static final String RELEASE_DATE = "2012-03-15";
+  public static final String RELEASE_DATE = "2012-03-30";
 
-  public static final String RELEASE_TIMESTAMP = "2012-03-15 02:01";
+  public static final String RELEASE_TIMESTAMP = "2012-03-30 15:26";
 }
 
 			
