4b24bdec93a3b56296cf8999fd90b9b261d25988
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-04-06 07:32:16 +0000

    Former tests of WicketUtilsTest added.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@11714 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/test/java/org/projectforge/web/UrlHelperTest.java
+++ src/test/java/org/projectforge/web/UrlHelperTest.java
@@ -41,5 +41,21 @@ public class UrlHelperTest
         URLHelper.removeJSessionId("http://localhost:8080/ProjectForge/wa/addressEdit;jsessionid=hji8ysdreqlz19bipa3ccm2jj"));
     assertEquals("http://localhost:8080/ProjectForge/wa/addressEdit?4",
         URLHelper.removeJSessionId("http://localhost:8080/ProjectForge/wa/addressEdit;jsessionid=hji8ysdreqlz19bipa3ccm2jj?4"));
+    assertEquals("https://localhost:8443/ProjectForge/wa/timesheetEdit/id/325295",
+        URLHelper
+        .removeJSessionId("https://localhost:8443/ProjectForge/wa/timesheetEdit/id/325295;jsessionid=DF6F216F10DC6A27EBA0EB60A7254EAA"));
+    assertEquals("https://localhost:8443/ProjectForge/wa/timesheetEdit/id/325295",
+        URLHelper.removeJSessionId("https://localhost:8443/ProjectForge/wa/timesheetEdit/id/325295"));
+    assertEquals("https://localhost:8443/?hurzel", URLHelper.removeJSessionId("https://localhost:8443/;jsessionid=376kjKJ224?hurzel"));
+    assertEquals(
+        "https://localhost:8443/ProjectForge/wa/?wicket:bookmarkablePage=:org.projectforge.web.timesheet.TimesheetMassUpdatePage",
+        URLHelper
+        .removeJSessionId("https://localhost:8443/ProjectForge/wa/;jsessionid=B2BE03E5838FDAFCE3ED1AE235A78878?wicket:bookmarkablePage=:org.projectforge.web.timesheet.TimesheetMassUpdatePage"));
+    assertEquals("https://localhost:8443/timesheetList/date=6546576/store=false?hurzel",
+        URLHelper.removeJSessionId("https://localhost:8443/timesheetList/date=6546576/store=false;jsessionid=376kjKJ224?hurzel"));
+    assertEquals("https://localhost:8443/timesheetList/?hurzel",
+        URLHelper.removeJSessionId("https://localhost:8443/timesheetList/;jsessionid=376kjKJ224?hurzel"));
+    assertEquals("https://localhost:8443/timesheetList/",
+        URLHelper.removeJSessionId("https://localhost:8443/timesheetList/;jsessionid=376kjKJ224"));
   }
 }
