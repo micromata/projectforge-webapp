bbefbab679d2f331665ad614025e61c85477d7f5
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-04-06 09:59:09 +0000

    Login button was renamed on LoginPage.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@11719 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/test/java/org/projectforge/web/wicket/WicketPageTestBase.java
+++ src/test/java/org/projectforge/web/wicket/WicketPageTestBase.java
@@ -117,7 +117,7 @@ public class WicketPageTestBase extends TestBase
     final FormTester form = tester.newFormTester("body:form");
     form.setValue("username", username);
     form.setValue("password", password);
-    form.submit("login");
+    form.submit("loginButton:button");
     tester.assertRenderedPage(WicketApplication.DEFAULT_PAGE);
   }
 
