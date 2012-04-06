f62f3995db99f6cd3bae366c44a3c61fa99507d5
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-04-06 07:30:49 +0000

    removeSessionId is moved to URLHelper.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@11711 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/test/java/org/projectforge/web/wicket/WicketUtilsTest.java
+++ src/test/java/org/projectforge/web/wicket/WicketUtilsTest.java
@@ -38,27 +38,6 @@ public class WicketUtilsTest
 {
 
   @Test
-  public void testRemoveSessionId()
-  {
-    assertEquals("https://localhost:8443/ProjectForge/wa/timesheetEdit/id/325295",
-        WicketUtils
-        .removeSessionId("https://localhost:8443/ProjectForge/wa/timesheetEdit/id/325295;jsessionid=DF6F216F10DC6A27EBA0EB60A7254EAA"));
-    assertEquals("https://localhost:8443/ProjectForge/wa/timesheetEdit/id/325295",
-        WicketUtils.removeSessionId("https://localhost:8443/ProjectForge/wa/timesheetEdit/id/325295"));
-    assertEquals("https://localhost:8443/?hurzel", WicketUtils.removeSessionId("https://localhost:8443/;jsessionid=376kjKJ224?hurzel"));
-    assertEquals(
-        "https://localhost:8443/ProjectForge/wa/?wicket:bookmarkablePage=:org.projectforge.web.timesheet.TimesheetMassUpdatePage",
-        WicketUtils
-        .removeSessionId("https://localhost:8443/ProjectForge/wa/;jsessionid=B2BE03E5838FDAFCE3ED1AE235A78878?wicket:bookmarkablePage=:org.projectforge.web.timesheet.TimesheetMassUpdatePage"));
-    assertEquals("https://localhost:8443/timesheetList/date=6546576/store=false?hurzel",
-        WicketUtils.removeSessionId("https://localhost:8443/timesheetList/date=6546576/store=false;jsessionid=376kjKJ224?hurzel"));
-    assertEquals("https://localhost:8443/timesheetList/?hurzel",
-        WicketUtils.removeSessionId("https://localhost:8443/timesheetList/;jsessionid=376kjKJ224?hurzel"));
-    assertEquals("https://localhost:8443/timesheetList/",
-        WicketUtils.removeSessionId("https://localhost:8443/timesheetList/;jsessionid=376kjKJ224"));
-  }
-
-  @Test
   public void createTooltip()
   {
     assertEquals("", WicketUtils.createTooltip(null, null));
