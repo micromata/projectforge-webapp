32026c176fbf3375a9153cad15526a467a69b462
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-04-14 23:00:18 +0000

    Mobile address pages enabled.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@11832 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/web/MenuItemRegistry.java
+++ src/main/java/org/projectforge/web/MenuItemRegistry.java
@@ -54,6 +54,7 @@ import org.projectforge.user.UserRightId;
 import org.projectforge.user.UserRightValue;
 import org.projectforge.web.access.AccessListPage;
 import org.projectforge.web.address.AddressListPage;
+import org.projectforge.web.address.AddressMobileListPage;
 import org.projectforge.web.address.PhoneCallPage;
 import org.projectforge.web.admin.AdminPage;
 import org.projectforge.web.admin.SystemUpdatePage;
@@ -244,7 +245,7 @@ public class MenuItemRegistry
     // COMMON
     reg.register(common, MenuItemDefId.CALENDAR, 10, CalendarPage.class); // Visible for all.
     reg.register(common, MenuItemDefId.BOOK_LIST, 30, BookListPage.class); // Visible for all.
-    reg.register(common, MenuItemDefId.ADDRESS_LIST, 40, AddressListPage.class);//.setMobileMenu(AddressMobileListPage.class, 100); // Visible for all.
+    reg.register(common, MenuItemDefId.ADDRESS_LIST, 40, AddressListPage.class).setMobileMenu(AddressMobileListPage.class, 100); // Visible for all.
     reg.register(common, MenuItemDefId.PHONE_CALL, 50, PhoneCallPage.class);
     final MenuItemDef meb = new MenuItemDef(common, MenuItemDefId.MEB.getId(), 50, MenuItemDefId.MEB.getI18nKey(), MebListPage.class) {
       @Override
