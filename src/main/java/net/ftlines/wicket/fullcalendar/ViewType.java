b58c2ecd4a0ccadfbfa450c4e32da63e967a7677
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-04-21 20:34:44 +0000

    Method getCode() added.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@11936 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/net/ftlines/wicket/fullcalendar/ViewType.java
+++ src/main/java/net/ftlines/wicket/fullcalendar/ViewType.java
@@ -16,24 +16,31 @@ package net.ftlines.wicket.fullcalendar;
 
 public enum ViewType
 {
-	MONTH("month"), BASIC_WEEK("basicWeek"), BASIC_DAY("basicDay"), AGENDA_WEEK("agendaWeek"), AGENDA_DAY("agendaDay");
+  MONTH("month"), BASIC_WEEK("basicWeek"), BASIC_DAY("basicDay"), AGENDA_WEEK("agendaWeek"), AGENDA_DAY("agendaDay");
 
-	private final String code;
+  private final String code;
 
-	private ViewType(String code)
-	{
-		this.code = code;
-	}
+  private ViewType(final String code)
+  {
+    this.code = code;
+  }
 
-	public static ViewType forCode(String code)
-	{
-		for (ViewType type : values())
-		{
-			if (type.code.equals(code))
-				return type;
-		}
-		throw new IllegalStateException("Invalid view type code: " + code);
-	}
+  public static ViewType forCode(final String code)
+  {
+    for (final ViewType type : values())
+    {
+      if (type.code.equals(code))
+        return type;
+    }
+    throw new IllegalStateException("Invalid view type code: " + code);
+  }
 
+  /**
+   * @return the code
+   */
+  public String getCode()
+  {
+    return code;
+  }
 
 }
