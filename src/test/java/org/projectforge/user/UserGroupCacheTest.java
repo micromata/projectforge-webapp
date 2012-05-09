39ec22c6c0754b4e790b54de45b419ac314ddb4c
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-05-09 22:04:50 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@12080 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/test/java/org/projectforge/user/UserGroupCacheTest.java
@@ -0,0 +1,82 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2012 Kai Reinhard (k.reinhard@micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.user;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+import org.projectforge.test.TestBase;
+
+public class UserGroupCacheTest extends TestBase
+{
+  private GroupDao groupDao;
+
+  private UserGroupCache userGroupCache;
+
+  public void setGroupDao(final GroupDao groupDao)
+  {
+    this.groupDao = groupDao;
+  }
+
+  /**
+   * @param userGroupCache the userGroupCache to set
+   * @return this for chaining.
+   */
+  public void setUserGroupCache(final UserGroupCache userGroupCache)
+  {
+    this.userGroupCache = userGroupCache;
+  }
+
+  @Test
+  public void testUserMemberOfAtLeastOneGroup()
+  {
+    logon(TEST_ADMIN_USER);
+    GroupDO group1 = new GroupDO();
+    group1.setName("testusergroupcache1");
+    Set<PFUserDO> assignedUsers = new HashSet<PFUserDO>();
+    group1.setAssignedUsers(assignedUsers);
+    assignedUsers.add(getUser(TEST_USER));
+    Serializable id = groupDao.save(group1);
+    group1 = groupDao.getById(id);
+
+    GroupDO group2 = new GroupDO();
+    group2.setName("testusergroupcache2");
+    assignedUsers = new HashSet<PFUserDO>();
+    group2.setAssignedUsers(assignedUsers);
+    assignedUsers.add(getUser(TEST_ADMIN_USER));
+    id = groupDao.save(group2);
+    group2 = groupDao.getById(id);
+
+    assertFalse(userGroupCache.isUserMemberOfAtLeastOneGroup(getUser(TEST_ADMIN_USER).getId()));
+    assertFalse(userGroupCache.isUserMemberOfAtLeastOneGroup(getUser(TEST_ADMIN_USER).getId(), group1.getId()));
+    assertTrue(userGroupCache.isUserMemberOfAtLeastOneGroup(getUser(TEST_ADMIN_USER).getId(), group2.getId()));
+    assertTrue(userGroupCache.isUserMemberOfAtLeastOneGroup(getUser(TEST_ADMIN_USER).getId(), group1.getId(), group2.getId()));
+    assertTrue(userGroupCache.isUserMemberOfAtLeastOneGroup(getUser(TEST_ADMIN_USER).getId(), group2.getId(), group1.getId()));
+  }
+}
