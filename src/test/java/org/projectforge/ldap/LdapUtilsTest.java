0eeea646e6b109b7e6333a5be6ef9aa906308a92
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-07-07 13:30:37 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@12185 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/test/java/org/projectforge/ldap/LdapUtilsTest.java
@@ -0,0 +1,58 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2012 Kai Reinhard (k.reinhard@micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.ldap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class LdapUtilsTest
+{
+  @Test
+  public void organizationalUnit()
+  {
+    assertNull(LdapUtils.getOrganizationalUnit(null));
+    assertNull(LdapUtils.getOrganizationalUnit(null, null));
+    assertNull(LdapUtils.getOrganizationalUnit(null, ""));
+    assertNull(LdapUtils.getOrganizationalUnit("cn=hurzel"));
+    assertNull(LdapUtils.getOrganizationalUnit("cn=hurzel", null));
+    assertNull(LdapUtils.getOrganizationalUnit("cn=hurzel", ""));
+    assertStringArray(LdapUtils.getOrganizationalUnit("cn=hurzel,ou=users", ""), "users");
+    assertStringArray(LdapUtils.getOrganizationalUnit("cn=hurzel", "ou=users"), "users");
+    assertStringArray(LdapUtils.getOrganizationalUnit("cn=hurzel,ou=intern,ou=users", ""), "intern", "users");
+    assertStringArray(LdapUtils.getOrganizationalUnit("cn=hurzel,ou=intern", "ou=users"), "intern", "users");
+    assertStringArray(LdapUtils.getOrganizationalUnit("cn=hurzel", "ou=intern,ou=users"), "intern", "users");
+  }
+
+  private void assertStringArray(final String[] array, final String... expected)
+  {
+    assertNotNull(array);
+    assertEquals(expected.length, array.length);
+    for (int i = 0; i < expected.length; i++) {
+      assertEquals(expected[i], array[i]);
+    }
+  }
+}
