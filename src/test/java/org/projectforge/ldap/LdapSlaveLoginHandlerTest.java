9854af034d2afd77fb94d711edef1c4eceae8995
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-08-18 05:53:46 +0000

    Panta rhei.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@12353 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/test/java/org/projectforge/ldap/LdapSlaveLoginHandlerTest.java
+++ src/test/java/org/projectforge/ldap/LdapSlaveLoginHandlerTest.java
@@ -29,6 +29,7 @@ import static org.mockito.Mockito.when;
 import org.junit.Assert;
 import org.junit.Test;
 import org.projectforge.test.TestBase;
+import org.projectforge.user.LoginResult;
 import org.projectforge.user.LoginResultStatus;
 import org.projectforge.user.PFUserDO;
 
@@ -39,6 +40,7 @@ public class LdapSlaveLoginHandlerTest extends TestBase
   {
     final String userBase = "ou=users";
     final LdapUserDao ldapUserDao = mock(LdapUserDao.class);
+    LoginResult loginResult;
     when(ldapUserDao.authenticate("kai", "successful", userBase)).thenReturn(true);
     when(ldapUserDao.authenticate("kai", "fail", userBase)).thenReturn(false);
     when(ldapUserDao.findByUsername("kai", userBase)).thenReturn(
@@ -52,20 +54,26 @@ public class LdapSlaveLoginHandlerTest extends TestBase
 
     Assert.assertFalse("User shouldn't be available yet in the data-base.",
         userDao.doesUsernameAlreadyExist(new PFUserDO().setUsername("kai")));
-    Assert.assertEquals(LoginResultStatus.SUCCESS, loginHandler.checkLogin("kai", "successful").getLoginResultStatus());
+    loginResult =loginHandler.checkLogin("kai", "successful");
+    Assert.assertEquals(LoginResultStatus.SUCCESS, loginResult.getLoginResultStatus());
+    assertUser(loginResult.getUser(), "kai", "Kai", "Reinhard", "k.reinhard@acme.com", "Micromata", "Developer");
     Assert.assertTrue("User should be created in data-base as a new user (in ldap).",
         userDao.doesUsernameAlreadyExist(new PFUserDO().setUsername("kai")));
     final PFUserDO user = userDao.getInternalByName("kai");
-    Assert.assertEquals("kai", user.getUsername());
+    assertUser(user, "kai", "Kai", "Reinhard", "k.reinhard@acme.com", "Micromata", "Developer");
     Assert.assertEquals(userDao.encryptPassword("successful"), user.getPassword());
-    Assert.assertEquals("Kai", user.getFirstname());
-    Assert.assertEquals("Reinhard", user.getLastname());
-    Assert.assertEquals("Micromata", user.getOrganization());
-    Assert.assertEquals("k.reinhard@acme.com", user.getEmail());
-    Assert.assertEquals("Developer", user.getDescription());
 
     userDao.internalMarkAsDeleted(user);
     Assert.assertEquals("User is deleted in data-base. Login not possible.", LoginResultStatus.LOGIN_EXPIRED,
         loginHandler.checkLogin("kai", "successful").getLoginResultStatus());
+  }
+
+  private void assertUser(final PFUserDO user, final String username, final String firstname, final String lastname, final String email, final String organization, final String description) {
+    Assert.assertEquals(username, user.getUsername());
+    Assert.assertEquals(firstname, user.getFirstname());
+    Assert.assertEquals(lastname, user.getLastname());
+    Assert.assertEquals(organization, user.getOrganization());
+    Assert.assertEquals(email, user.getEmail());
+    Assert.assertEquals(description, user.getDescription());
   }
 }
