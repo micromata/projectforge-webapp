a0121e2f01beee6f5fa18653d6875dedd8917f39
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-09-27 00:24:19 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@12509 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/web/user/UsersProvider.java
@@ -0,0 +1,154 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2012 Kai Reinhard (k.reinhard@micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.user;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.projectforge.common.NumberHelper;
+import org.projectforge.registry.Registry;
+import org.projectforge.user.PFUserDO;
+import org.projectforge.user.UserGroupCache;
+
+import com.vaynberg.wicket.select2.Response;
+import com.vaynberg.wicket.select2.TextChoiceProvider;
+
+public class UsersProvider extends TextChoiceProvider<PFUserDO>
+{
+  private static final long serialVersionUID = 6228672635966093252L;
+
+  private transient UserGroupCache userGroupCache;
+
+  private int pageSize = 20;
+
+  private final UsersComparator usersComparator = new UsersComparator();
+
+  public SortedSet<PFUserDO> getSortedUsers()
+  {
+    final Collection<PFUserDO> allusers = getUserGroupCache().getAllUsers();
+    final SortedSet<PFUserDO> sortedusers = new TreeSet<PFUserDO>(usersComparator);
+    for (final PFUserDO user : allusers) {
+      if (user.isDeleted() == false) {
+        sortedusers.add(user);
+      }
+    }
+    return sortedusers;
+  }
+
+  /**
+   * @param pageSize the pageSize to set
+   * @return this for chaining.
+   */
+  public UsersProvider setPageSize(final int pageSize)
+  {
+    this.pageSize = pageSize;
+    return this;
+  }
+
+  /**
+   * @see com.vaynberg.wicket.select2.TextChoiceProvider#getDisplayText(java.lang.Object)
+   */
+  @Override
+  protected String getDisplayText(final PFUserDO choice)
+  {
+    return choice.getFullname();
+  }
+
+  /**
+   * @see com.vaynberg.wicket.select2.TextChoiceProvider#getId(java.lang.Object)
+   */
+  @Override
+  protected Object getId(final PFUserDO choice)
+  {
+    return choice.getId();
+  }
+
+  /**
+   * @see com.vaynberg.wicket.select2.ChoiceProvider#query(java.lang.String, int, com.vaynberg.wicket.select2.Response)
+   */
+  @Override
+  public void query(String term, final int page, final Response<PFUserDO> response)
+  {
+    final Set<PFUserDO> sortedUsers = getSortedUsers();
+    final List<PFUserDO> result = new ArrayList<PFUserDO>();
+    term = term.toLowerCase();
+
+    final int offset = page * pageSize;
+
+    int matched = 0;
+    boolean hasMore = false;
+    for (final PFUserDO user : sortedUsers) {
+      if (result.size() == pageSize) {
+        hasMore = true;
+        break;
+      }
+      if (user.getFullname().toLowerCase().contains(term) == true || user.getUsername().toLowerCase().contains(term) == true) {
+        matched++;
+        if (matched > offset) {
+          result.add(user);
+        }
+      }
+    }
+    response.addAll(result);
+    response.setHasMore(hasMore);
+  }
+
+  /**
+   * @see com.vaynberg.wicket.select2.ChoiceProvider#toChoices(java.util.Collection)
+   */
+  @Override
+  public Collection<PFUserDO> toChoices(final Collection<String> ids)
+  {
+    final List<PFUserDO> list = new ArrayList<PFUserDO>();
+    if (ids == null) {
+      return list;
+    }
+    for (final String str : ids) {
+      final Integer userId = NumberHelper.parseInteger(str);
+      if (userId == null) {
+        continue;
+      }
+      final PFUserDO user = getUserGroupCache().getUser(userId);
+      if (user != null) {
+        list.add(user);
+      }
+    }
+    return list;
+  }
+
+  /**
+   * @return the useruserCache
+   */
+  private UserGroupCache getUserGroupCache()
+  {
+    if (userGroupCache == null) {
+      userGroupCache = Registry.instance().getUserGroupCache();
+    }
+    return userGroupCache;
+  }
+}
