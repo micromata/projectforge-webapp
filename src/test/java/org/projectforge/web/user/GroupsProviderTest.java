6deec3854575cd1bcc2bef331707482d1793a3f3
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-09-27 06:50:10 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@12516 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/test/java/org/projectforge/web/user/GroupsProviderTest.java
@@ -0,0 +1,95 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2012 Kai Reinhard (k.reinhard@micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.user;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.projectforge.user.GroupDO;
+import org.projectforge.user.UserGroupCache;
+
+public class GroupsProviderTest
+{
+  @Test
+  public void convertGroupIds()
+  {
+    final GroupsProvider groupsProvider = new GroupsProvider();
+    groupsProvider.userGroupCache = Mockito.mock(UserGroupCache.class);
+    Mockito.when(groupsProvider.userGroupCache.getGroup(1)).thenReturn(cg("1", 1));
+    Mockito.when(groupsProvider.userGroupCache.getGroup(2)).thenReturn(cg("2", 2));
+    Mockito.when(groupsProvider.userGroupCache.getGroup(3)).thenReturn(cg("3", 3));
+    Mockito.when(groupsProvider.userGroupCache.getGroup(4)).thenReturn(cg("4", 4));
+
+    Assert.assertEquals("", groupsProvider.getGroupIds(createGroupsCol()));
+    Assert.assertEquals("1", groupsProvider.getGroupIds(createGroupsCol(1)));
+    Assert.assertEquals("1,2", groupsProvider.getGroupIds(createGroupsCol(1, 2)));
+    Assert.assertEquals("1,2,3", groupsProvider.getGroupIds(createGroupsCol(3, 1, 2)));
+
+    assertGroupSet(groupsProvider.getSortedGroups(""));
+    assertGroupSet(groupsProvider.getSortedGroups(" ,, ,"));
+    assertGroupSet(groupsProvider.getSortedGroups("1"), 1);
+    assertGroupSet(groupsProvider.getSortedGroups("3,1"), 1, 3);
+    assertGroupSet(groupsProvider.getSortedGroups("3,1,2,4"), 1, 2, 3, 4);
+  }
+
+  /**
+   * Creates a group with the given name and id.
+   * @param name
+   * @param id
+   */
+  private GroupDO cg(final String name, final int id)
+  {
+    final GroupDO group = new GroupDO();
+    group.setName(name).setId(id);
+    return group;
+  }
+
+  private Collection<GroupDO> createGroupsCol(final int... groupIds)
+  {
+    final Collection<GroupDO> col = new TreeSet<GroupDO>(new GroupsComparator());
+    for (final int id : groupIds) {
+      col.add(cg(String.valueOf(id), id));
+    }
+    return col;
+  }
+
+  private void assertGroupSet(final SortedSet<GroupDO> actualGroupSet, final int... expectedIds)
+  {
+    Assert.assertEquals(expectedIds.length, actualGroupSet.size());
+    final Set<Integer> actualIdSet = new HashSet<Integer>();
+    for (final GroupDO actualGroup : actualGroupSet) {
+      actualIdSet.add(actualGroup.getId());
+    }
+    for (final int expectedId : expectedIds) {
+      Assert.assertTrue(actualIdSet.contains(expectedId));
+    }
+  }
+}
