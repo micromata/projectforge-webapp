e895443aee2b6ec545a11c2b369c0de697909b48
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-10-08 19:25:05 +0000

    TeamCalendarPage added.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@12534 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/plugins/teamcal/TeamCalPlugin.java
+++ src/main/java/org/projectforge/plugins/teamcal/TeamCalPlugin.java
@@ -78,7 +78,8 @@ public class TeamCalPlugin extends AbstractPlugin
 
     // Register the menu entry as sub menu entry of the misc menu:
     final MenuItemDef parentMenu = getMenuItemDef(MenuItemDefId.MISC);
-    registerMenuItem(new MenuItemDef(parentMenu, ID, 7, "plugins.teamcal.menu", TeamCalListPage.class));
+    registerMenuItem(new MenuItemDef(parentMenu, ID, 7, "plugins.teamcal.menu", TeamCalendarPage.class));
+    registerMenuItem(new MenuItemDef(parentMenu, ID, 8, "plugins.teamcal.title.list", TeamCalListPage.class));
 
     // .setMobileMenu(ToDoMobileListPage.class, 10));
 
