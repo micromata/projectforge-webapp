ea7e8b2f8c23d2e6033bca9447f35447ace07d22
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-10-22 22:05:44 +0000

    Nested groups removed.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@12622 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/database/InitDatabaseDao.java
+++ src/main/java/org/projectforge/database/InitDatabaseDao.java
@@ -187,7 +187,7 @@ public class InitDatabaseDao extends HibernateDaoSupport
     group.setName(projectForgeGroup.toString());
     group.setDescription(description);
     group.setAssignedUsers(users);
-    group.setNestedGroupsAllowed(false);
+    //group.setNestedGroupsAllowed(false);
     group.setLocalGroup(true); // Do not synchronize group with external user management system by default.
     groupDao.internalSave(group);
   }
