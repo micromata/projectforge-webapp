e32e278a1ecbe69344aa92e1e2e17a268e10c796
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-10-26 21:41:47 +0000

    getOuBase() implemented.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@12656 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/filerepository/FileDO.java
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2012 Kai Reinhard (k.reinhard@micromata.com)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.filerepository;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.search.annotations.Indexed;
+import org.projectforge.core.DefaultBaseDO;
+
+/**
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ * 
+ */
+@Entity
+@Indexed
+@Table(name = "T_FILE")
+public class FileDO extends DefaultBaseDO
+{
+  private static final long serialVersionUID = 8036741307214351813L;
+}
