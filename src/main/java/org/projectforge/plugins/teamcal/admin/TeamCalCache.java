73c67b08859ea0aa60c8e131bbe2e0d0e1c79605
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-12-09 18:02:31 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@12983 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/plugins/teamcal/admin/TeamCalCache.java
@@ -0,0 +1,102 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2012 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.teamcal.admin;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.apache.log4j.Logger;
+import org.projectforge.common.AbstractCache;
+import org.projectforge.registry.Registry;
+import org.projectforge.user.PFUserContext;
+import org.projectforge.user.UserRights;
+
+/**
+ * Caches all calendars.
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ */
+public class TeamCalCache extends AbstractCache
+{
+  private static Logger log = Logger.getLogger(TeamCalCache.class);
+
+  private static TeamCalCache instance = new TeamCalCache();
+
+  private transient TeamCalDao teamCalDao;
+
+  private transient TeamCalRight teamCalRight;
+
+  private Map<Integer, TeamCalDO> calendarMap;
+
+  public static TeamCalCache getInstance()
+  {
+    return instance;
+  }
+
+  public TeamCalDO getCalendar(final Integer calendarId)
+  {
+    checkRefresh();
+    return calendarMap.get(calendarId);
+  }
+
+  /**
+   * Get ordered calendars (by title and id).
+   * @return All accessible calendars of the context user (as owner or with full, read-only or minimal access).
+   */
+  public Collection<TeamCalDO> getAllAccessibleCalendars()
+  {
+    checkRefresh();
+    final Set<TeamCalDO> set = new TreeSet<TeamCalDO>(new TeamCalsComparator());
+    for (final TeamCalDO cal : calendarMap.values()) {
+      if (teamCalRight.hasSelectAccess(PFUserContext.getUser()) == true) {
+        set.add(cal);
+      }
+    }
+    return set;
+  }
+
+  /**
+   * This method will be called by CacheHelper and is synchronized via getData();
+   */
+  @Override
+  protected void refresh()
+  {
+    log.info("Initializing TeamCalCache ...");
+    if (teamCalDao == null || teamCalRight == null) {
+      teamCalDao = Registry.instance().getDao(TeamCalDao.class);
+      teamCalRight = (TeamCalRight) UserRights.instance().getRight(TeamCalDao.USER_RIGHT_ID);
+    }
+    // This method must not be synchronized because it works with a new copy of maps.
+    final Map<Integer, TeamCalDO> map = new HashMap<Integer, TeamCalDO>();
+    final List<TeamCalDO> list = teamCalDao.internalLoadAll();
+    for (final TeamCalDO cal : list) {
+      map.put(cal.getId(), cal);
+    }
+    this.calendarMap = map;
+    log.info("Initializing of TeamCalCache done.");
+  }
+}
---
+++ src/main/java/org/projectforge/plugins/teamcal/admin/TeamCalsProvider.java
@@ -0,0 +1,213 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2012 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.teamcal.admin;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.TreeSet;
+
+import org.apache.commons.lang.StringUtils;
+import org.projectforge.common.NumberHelper;
+import org.projectforge.common.StringHelper;
+
+import com.vaynberg.wicket.select2.Response;
+import com.vaynberg.wicket.select2.TextChoiceProvider;
+
+public class TeamCalsProvider extends TextChoiceProvider<TeamCalDO>
+{
+  private static final long serialVersionUID = -7219524032951522997L;
+
+  private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(TeamCalsProvider.class);
+
+  private int pageSize = 20;
+
+  private final TeamCalsComparator calsComparator = new TeamCalsComparator();
+
+  private Collection<TeamCalDO> sortedCals;
+
+  private TeamCalCache teamCalCache;
+
+  /**
+   * @param calIds
+   * @return
+   */
+  public List<String> getCalendarNames(final String calIds)
+  {
+    if (StringUtils.isEmpty(calIds) == true) {
+      return null;
+    }
+    final int[] ids = StringHelper.splitToInts(calIds, ",", false);
+    final List<String> list = new ArrayList<String>();
+    for (final int id : ids) {
+      final TeamCalDO cal = getTeamCalCache().getCalendar(id);
+      if (cal != null) {
+        list.add(cal.getTitle());
+      } else {
+        log.warn("TeamCalDO with id '" + id + "' not found. calIds string was: " + calIds);
+      }
+    }
+    return list;
+  }
+
+  /**
+   * 
+   * @param calIds
+   * @return
+   */
+  public Collection<TeamCalDO> getSortedCalendars(final String calIds)
+  {
+    if (StringUtils.isEmpty(calIds) == true) {
+      return null;
+    }
+    sortedCals = new TreeSet<TeamCalDO>(calsComparator);
+    final int[] ids = StringHelper.splitToInts(calIds, ",", false);
+    for (final int id : ids) {
+      final TeamCalDO cal = getTeamCalCache().getCalendar(id);
+      if (cal != null) {
+        sortedCals.add(cal);
+      } else {
+        log.warn("TeamCalDO with id '" + id + "' not found. calIds string was: " + calIds);
+      }
+    }
+    return sortedCals;
+  }
+
+  public String getCalendarIds(final Collection<TeamCalDO> calendars)
+  {
+    final StringBuffer buf = new StringBuffer();
+    boolean first = true;
+    for (final TeamCalDO calendar : calendars) {
+      if (calendar.getId() != null) {
+        first = StringHelper.append(buf, first, String.valueOf(calendar.getId()), ",");
+      }
+    }
+    return buf.toString();
+  }
+
+  public Collection<TeamCalDO> getSortedCalenders()
+  {
+    if (sortedCals == null) {
+      final Collection<TeamCalDO> allCalendars = getTeamCalCache().getAllAccessibleCalendars();
+      sortedCals = new TreeSet<TeamCalDO>(calsComparator);
+      for (final TeamCalDO cal : allCalendars) {
+        if (cal.isDeleted() == false) {
+          sortedCals.add(cal);
+        }
+      }
+    }
+    return sortedCals;
+  }
+
+  /**
+   * @param pageSize the pageSize to set
+   * @return this for chaining.
+   */
+  public TeamCalsProvider setPageSize(final int pageSize)
+  {
+    this.pageSize = pageSize;
+    return this;
+  }
+
+  /**
+   * @see com.vaynberg.wicket.select2.TextChoiceProvider#getDisplayText(java.lang.Object)
+   */
+  @Override
+  protected String getDisplayText(final TeamCalDO choice)
+  {
+    return choice.getTitle();
+  }
+
+  /**
+   * @see com.vaynberg.wicket.select2.TextChoiceProvider#getId(java.lang.Object)
+   */
+  @Override
+  protected Object getId(final TeamCalDO choice)
+  {
+    return choice.getId();
+  }
+
+  /**
+   * @see com.vaynberg.wicket.select2.ChoiceProvider#query(java.lang.String, int, com.vaynberg.wicket.select2.Response)
+   */
+  @Override
+  public void query(String term, final int page, final Response<TeamCalDO> response)
+  {
+    final Collection<TeamCalDO> sortedCals = getSortedCalenders();
+    final List<TeamCalDO> result = new ArrayList<TeamCalDO>();
+    term = term.toLowerCase();
+
+    final int offset = page * pageSize;
+
+    int matched = 0;
+    boolean hasMore = false;
+    for (final TeamCalDO cal : sortedCals) {
+      if (result.size() == pageSize) {
+        hasMore = true;
+        break;
+      }
+      final String title = cal.getTitle();
+      if (title != null && title.toLowerCase().contains(term) == true) {
+        matched++;
+        if (matched > offset) {
+          result.add(cal);
+        }
+      }
+    }
+    response.addAll(result);
+    response.setHasMore(hasMore);
+  }
+
+  /**
+   * @see com.vaynberg.wicket.select2.ChoiceProvider#toChoices(java.util.Collection)
+   */
+  @Override
+  public Collection<TeamCalDO> toChoices(final Collection<String> ids)
+  {
+    final List<TeamCalDO> list = new ArrayList<TeamCalDO>();
+    if (ids == null) {
+      return list;
+    }
+    for (final String str : ids) {
+      final Integer calId = NumberHelper.parseInteger(str);
+      if (calId == null) {
+        continue;
+      }
+      final TeamCalDO cal = getTeamCalCache().getCalendar(calId);
+      if (cal != null) {
+        list.add(cal);
+      }
+    }
+    return list;
+  }
+
+  private TeamCalCache getTeamCalCache()
+  {
+    if (teamCalCache == null) {
+      // Mustn't be synchronized, nothings happens if assigned multiple times.
+      teamCalCache = TeamCalCache.getInstance();
+    }
+    return teamCalCache;
+  }
+}
