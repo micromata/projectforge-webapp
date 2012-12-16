93833f6eac01c00037dd29b5291e0b7e9da3ef0a
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-12-16 22:23:02 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@13031 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/plugins/teamcal/TeamCalConfig.java
@@ -0,0 +1,62 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2012 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.teamcal;
+
+import org.projectforge.core.ConfigXml;
+import org.projectforge.core.ConfigurationData;
+
+public class TeamCalConfig implements ConfigurationData
+{
+  private static TeamCalConfig config;
+
+  public static TeamCalConfig get()
+  {
+    if (config == null) {
+      config = (TeamCalConfig)ConfigXml.getInstance().getPluginConfig(TeamCalConfig.class);
+    }
+    return config;
+  }
+
+  private String domain = "projectforge.acme.priv";
+
+  /**
+   * The domain is needed for having world wide unique uids. Please note: If you change this domain after working with this plugin,
+   * synchronisation errors between ProjectForge and your iCal clients may occur.
+   * @return the domain
+   */
+  public String getDomain()
+  {
+    return domain;
+  }
+
+  /**
+   * @param domain the domain to set
+   * @return this for chaining.
+   */
+  public TeamCalConfig setDomain(final String domain)
+  {
+    this.domain = domain;
+    return this;
+  }
+}
