0aeaf273783805a6b8311b996ad6a38a736b2ab2
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2012-12-27 00:39:20 +0000

    Initial revision.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@13211 be79319d-3443-0410-9b0a-b0c1b946f33b

---
+++ src/main/java/org/projectforge/web/wicket/bootstrap/GridType.java
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2012 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.wicket.bootstrap;
+
+import org.projectforge.common.StringHelper;
+
+/**
+ * Used for defining class attribute value for elements.
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ */
+public enum GridType
+{
+  OFFSET1("offset1"), OFFSET2("offste2"), OFFSET3("offset3"), ROW("row"), ROW_FLUID("row-fluid"), SPAN1("span1"), SPAN2("span2"), SPAN3(
+      "span3"), SPAN4("span4"), SPAN6("span6"), SPAN8("span8"), SPAN12("span12");
+
+  private String classAttrValue;
+
+  public static String gridTypesToClassString(final GridType... gridTypes)
+  {
+    if (gridTypes == null || gridTypes.length == 0) {
+      return null;
+    }
+    if (gridTypes.length == 1) {
+      return gridTypes[0].classAttrValue;
+    }
+    boolean first = true;
+    final StringBuffer buf = new StringBuffer();
+    for (final GridType gridType : gridTypes) {
+      first = StringHelper.append(buf, first, gridType.classAttrValue, " ");
+    }
+    return buf.toString();
+  }
+
+  public String getClassAttrValue()
+  {
+    return classAttrValue;
+  }
+
+  private GridType(final String classAttrValue)
+  {
+    this.classAttrValue = classAttrValue;
+  }
+}
