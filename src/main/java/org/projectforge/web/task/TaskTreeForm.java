4971b3ba78236dd3ff36e0eeea813b2a749ec155
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2013-01-12 18:56:31 +0000

    Adapted to new icon handling of FieldsetPanel.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@13532 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/web/task/TaskTreeForm.java
+++ src/main/java/org/projectforge/web/task/TaskTreeForm.java
@@ -80,7 +80,7 @@ public class TaskTreeForm extends AbstractForm<TaskFilter, TaskTreePage>
       searchField.add(WicketUtils.setFocus());
       WicketUtils.addTooltip(searchField, getString("search.string.info.title"), getParentPage().getSearchToolTip());
       fs.add(new InputPanel(fs.newChildId(), searchField));
-      fs.add(new IconPanel(fs.newChildId(), IconType.HELP, getString("tooltip.lucene.link")).setOnClickLocation(
+      fs.add(new IconPanel(fs.newIconChildId(), IconType.HELP, getString("tooltip.lucene.link")).setOnClickLocation(
           WebConstants.DOC_LINK_HANDBUCH_LUCENE, true), FieldSetIconPosition.TOP_RIGHT);
     }
     {
