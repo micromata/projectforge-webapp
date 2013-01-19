b8c5a6edb38e226f0e0e039a7cb53220891496e2
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2013-01-19 23:56:21 +0000

    New smaller version of jquery-ui.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@13690 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/web/wicket/WicketRenderHeadUtils.java
+++ src/main/java/org/projectforge/web/wicket/WicketRenderHeadUtils.java
@@ -33,7 +33,8 @@ public class WicketRenderHeadUtils
   private static final String[][] JAVASCRIPT_FILES_DEF = { //
     // { "scripts/jquery/1.8.2/jquery-1.8.2", ".min"}, // Wicket delivers jQuery version
     { "include/bootstrap/js/bootstrap", ".min"}, //
-    { "scripts/jqueryui/1.9.2/jquery-ui-1.9.2.custom", ".min"}, //
+    // Needed: core, widget, button, datepicker:
+    { "scripts/jqueryui/jquery-ui-1.10.0.custom", ".min"}, //
     { "scripts/contextmenu/jquery.contextmenu", ""}, //
     { "scripts/adminica-2.2/adminica_ui", ""}, // modified!
     { "scripts/projectforge", ""} //
@@ -44,7 +45,7 @@ public class WicketRenderHeadUtils
   private static final String[][] CSS_FILES_DEF = { //
     { "styles/google-fonts/google-fonts", ""}, //
     // "http://fonts.googleapis.com/css?family=Droid+Sans:regular&amp;subset=latin", //
-    { "styles/jqueryui/1.9.2/cupertino/jquery-ui-1.9.2.custom", ".min"}, //
+    { "styles/jqueryui/1.10.0/ui-lightness/jquery-ui-1.10.0.custom", ".min"}, //
     { "styles/table", ""}, //
     { "scripts/contextmenu/css/jquery.contextmenu", ""}, //
     { "styles/adminica-2.2/main", ""}, //
