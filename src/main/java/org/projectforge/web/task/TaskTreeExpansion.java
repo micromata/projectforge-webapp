af3228d32d819fc991ae82caa76b1ca7005296be
kai <kai@be79319d-3443-0410-9b0a-b0c1b946f33b>
2013-01-21 01:08:02 +0000

    Don't need to store Expansion in Session.
    git-svn-id: https://svn.micromata.de/svn/projectforge/webapp/trunk@13734 be79319d-3443-0410-9b0a-b0c1b946f33b

--- src/main/java/org/projectforge/web/task/TaskTreeExpansion.java
+++ src/main/java/org/projectforge/web/task/TaskTreeExpansion.java
@@ -25,8 +25,6 @@ package org.projectforge.web.task;
 
 import java.util.Set;
 
-import org.apache.wicket.MetaDataKey;
-import org.apache.wicket.Session;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.projectforge.task.TaskNode;
@@ -49,22 +47,18 @@ public class TaskTreeExpansion extends TableTreeExpansion<Integer, TaskNode>
   private static TaskTreeExpansion get()
   {
     final UserXmlPreferencesCache userXmlPreferencesCache = UserXmlPreferencesCache.getDefaultInstance();
-    TaskTreeExpansion expansion = Session.get().getMetaData(KEY);
-    if (expansion == null) {
-      expansion = new TaskTreeExpansion();
-      Session.get().setMetaData(KEY, expansion);
-      try {
-        @SuppressWarnings("unchecked")
-        final Set<Integer> ids = (Set<Integer>) userXmlPreferencesCache.getEntry(TaskTreePage.USER_PREFS_KEY_OPEN_TASKS);
-        if (ids != null) {
-          expansion.setIds(ids);
-        } else {
-          // Persist the open entries in the data-base.
-          userXmlPreferencesCache.putEntry(TaskTreePage.USER_PREFS_KEY_OPEN_TASKS, expansion.getIds(), true);
-        }
-      } catch (final Exception ex) {
-        log.error(ex.getMessage(), ex);
+    final TaskTreeExpansion expansion = new TaskTreeExpansion();
+    try {
+      @SuppressWarnings("unchecked")
+      final Set<Integer> ids = (Set<Integer>) userXmlPreferencesCache.getEntry(TaskTreePage.USER_PREFS_KEY_OPEN_TASKS);
+      if (ids != null) {
+        expansion.setIds(ids);
+      } else {
+        // Persist the open entries in the data-base.
+        userXmlPreferencesCache.putEntry(TaskTreePage.USER_PREFS_KEY_OPEN_TASKS, expansion.getIds(), true);
       }
+    } catch (final Exception ex) {
+      log.error(ex.getMessage(), ex);
     }
     return expansion;
   }
@@ -86,8 +80,4 @@ public class TaskTreeExpansion extends TableTreeExpansion<Integer, TaskNode>
       }
     };
   }
-
-  private static MetaDataKey<TaskTreeExpansion> KEY = new MetaDataKey<TaskTreeExpansion>() {
-    private static final long serialVersionUID = 1L;
-  };
 }
