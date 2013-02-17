15a371afa442f959d523ffbc0fbc175196b27f81
Kai Reinhard <K.Reinhard@micromata.de>
2013-02-17 17:44:26 +0100

    Initial revision.

---
+++ src/test/java/org/projectforge/plugins/teamcal/TeamCalConfigTest.java
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2013 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.teamcal;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TeamCalConfigTest
+{
+  @Test
+  public void extractUid()
+  {
+    final TeamCalConfig config = new TeamCalConfig();
+    config.domain = "projectforge.org";
+    Assert.assertNull(config.extractEventId(null));
+    Assert.assertNull(config.extractEventId(""));
+    Assert.assertNull(config.extractEventId("unkown-123@" + config.domain));
+    Assert.assertEquals(new Integer("123"), config.extractEventId(TeamCalConfig.EVENT_UID_PREFIX + "-123@" + config.domain));
+    Assert.assertEquals(new Integer("1"), config.extractEventId(TeamCalConfig.EVENT_UID_PREFIX + "-1@" + config.domain));
+  }
+}
