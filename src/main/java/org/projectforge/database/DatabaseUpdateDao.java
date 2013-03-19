a1c8c96bdcab373d14d63ea8a510348d51d1bf20
Kai Reinhard <K.Reinhard@micromata.de>
2013-03-19 06:27:26 +0100

    Reverted to last revision.

--- src/main/java/org/projectforge/database/DatabaseUpdateDao.java
+++ src/main/java/org/projectforge/database/DatabaseUpdateDao.java
@@ -268,11 +268,6 @@ public class DatabaseUpdateDao
     }
     final StringBuffer buf = new StringBuffer();
     buildCreateTableStatement(buf, table);
-    for (final TableAttribute attr : table.getAttributes()) {
-      if (attr.getForeignTable() != null) {
-        buildForeignKeyConstraint(buf, table.getName(), attr);
-      }
-    }
     execute(buf.toString());
     return true;
   }
@@ -286,6 +281,11 @@ public class DatabaseUpdateDao
       buf.append("ALTER TABLE ").append(table).append(" ADD COLUMN ");
       buildAttribute(buf, attr);
       buf.append(";\n");
+    }
+    for (final TableAttribute attr : attributes) {
+      if (attr.getForeignTable() != null) {
+        buildForeignKeyConstraint(buf, table, attr);
+      }
     }
   }
 
