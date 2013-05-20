c2a804a847bb60b1200ed7b6ae432986d41c3264
Kai Reinhard <K.Reinhard@micromata.de>
2013-05-20 20:13:27 +0200

    Initial revision.

---
+++ src/main/java/org/projectforge/web/rest/converter/DOConverter.java
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2013 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// task community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// task community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with task program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.rest.converter;
+
+import org.projectforge.core.DefaultBaseDO;
+import org.projectforge.web.rest.AbstractBaseObject;
+
+/**
+ * For conversion of DefaultBaseDO to base object.
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ * 
+ */
+public class DOConverter
+{
+  public static void copyFields(final AbstractBaseObject dest, final DefaultBaseDO src)
+  {
+    dest.setId(src.getId());
+    dest.setDeleted(src.isDeleted());
+    dest.setCreated(src.getCreated());
+    dest.setLastUpdate(src.getLastUpdate());
+  }
+}
---
+++ src/main/java/org/projectforge/web/rest/converter/TaskDOConverter.java
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2013 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// task community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// task community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with task program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.web.rest.converter;
+
+import org.projectforge.task.TaskDO;
+import org.projectforge.web.rest.TaskObject;
+
+/**
+ * For conversion of TaskDO to task object.
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ * 
+ */
+public class TaskDOConverter
+{
+  public static TaskObject getTaskObject(final TaskDO taskDO)
+  {
+    final TaskObject task = new TaskObject();
+    DOConverter.copyFields(task, taskDO);
+    task.setParentTaskId(taskDO.getParentTaskId());
+    task.setDescription(taskDO.getDescription());
+    task.setReference(taskDO.getReference());
+    task.setTitle(taskDO.getTitle());
+    task.setShortDescription(taskDO.getShortDescription());
+    task.setMaxHours(taskDO.getMaxHours());
+    task.setPriority(taskDO.getPriority());
+    task.setStatus(taskDO.getStatus());
+    return task;
+  }
+}
