fb00a906fc193ae63c856af645983175f286c587
Kai Reinhard <K.Reinhard@micromata.de>
2013-06-08 06:18:03 +0200

    Initial revision.

---
+++ src/main/java/org/projectforge/plugins/liquidityplanning/LiquidityEntryDao.java
@@ -0,0 +1,58 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2013 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.liquidityplanning;
+
+import org.projectforge.core.BaseDao;
+import org.projectforge.user.UserRightId;
+
+/**
+ * 
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ * 
+ */
+public class LiquidityEntryDao extends BaseDao<LiquidityEntryDO>
+{
+  public static final UserRightId USER_RIGHT_ID = new UserRightId("PLUGIN_LIQUIDITY_PLANNING", "fibu10", "plugins.liquidityplanning.menu");;
+
+  public LiquidityEntryDao()
+  {
+    super(LiquidityEntryDO.class);
+    userRightId = USER_RIGHT_ID;
+  }
+
+  @Override
+  public LiquidityEntryDO newInstance()
+  {
+    return new LiquidityEntryDO();
+  }
+
+  /**
+   * @see org.projectforge.core.BaseDao#useOwnCriteriaCacheRegion()
+   */
+  @Override
+  protected boolean useOwnCriteriaCacheRegion()
+  {
+    return true;
+  }
+}
