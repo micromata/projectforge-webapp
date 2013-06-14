794d5749215d8f8edac63988688a934d46d7339f
Kai Reinhard <K.Reinhard@micromata.de>
2013-06-14 23:33:24 +0200

    Initial revision.

---
+++ src/main/java/org/projectforge/export/MyExcelExporter.java
@@ -0,0 +1,80 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2013 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.export;
+
+import java.lang.reflect.Field;
+
+import org.projectforge.common.DateFormatType;
+import org.projectforge.common.DateFormats;
+import org.projectforge.core.PropertyInfo;
+import org.projectforge.core.PropertyType;
+import org.projectforge.excel.ContentProvider;
+import org.projectforge.excel.ExcelExporter;
+import org.projectforge.excel.ExportColumn;
+
+/**
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ * 
+ */
+public class MyExcelExporter extends ExcelExporter
+{
+  /**
+   * @param filename
+   */
+  public MyExcelExporter(final String filename)
+  {
+    super(filename);
+  }
+
+  /**
+   * Adds customized formats. Put here your customized formats to your ExportSheet.
+   * @param field
+   * @param propInfo may-be null.
+   * @param column
+   * @return true, if format is handled by this method, otherwise false.
+   */
+  @Override
+  protected void putFieldFormat(final ContentProvider sheetProvider, final Field field, final PropertyInfo propInfo,
+      final ExportColumn exportColumn)
+  {
+    final PropertyType type = propInfo.type();
+    if (type == PropertyType.DATE) {
+      sheetProvider.putFormat(exportColumn, DateFormats.getExcelFormatString(DateFormatType.DATE));
+      return;
+    } else if (type == PropertyType.DATE_TIME) {
+      sheetProvider.putFormat(exportColumn, DateFormats.getExcelFormatString(DateFormatType.DATE_TIME_MINUTES));
+    } else if (type == PropertyType.DATE_TIME_SECONDS) {
+      sheetProvider.putFormat(exportColumn, DateFormats.getExcelFormatString(DateFormatType.DATE_TIME_SECONDS));
+    } else if (type == PropertyType.DATE_TIME_MILLIS) {
+      sheetProvider.putFormat(exportColumn, DateFormats.getExcelFormatString(DateFormatType.DATE_TIME_MILLIS));
+    } else if (type == PropertyType.UNSPECIFIED) {
+      if (java.sql.Date.class.isAssignableFrom(field.getType()) == true) {
+        sheetProvider.putFormat(exportColumn, DateFormats.getExcelFormatString(DateFormatType.DATE));
+      } else if (java.util.Date.class.isAssignableFrom(field.getType()) == true) {
+        sheetProvider.putFormat(exportColumn, DateFormats.getExcelFormatString(DateFormatType.DATE_TIME_MINUTES));
+      }
+    }
+    super.putFieldFormat(sheetProvider, field, propInfo, exportColumn);
+  }
+}
