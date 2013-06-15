91fd69223db4c2787cb42e4e394d9fa47fc9d805
Kai Reinhard <K.Reinhard@micromata.de>
2013-06-15 23:51:05 +0200

    Initial revision.

---
+++ src/main/java/org/projectforge/fibu/InvoicesExcelExport.java
@@ -0,0 +1,106 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2013 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.fibu;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import org.projectforge.core.PropUtils;
+import org.projectforge.core.PropertyInfo;
+import org.projectforge.excel.ContentProvider;
+import org.projectforge.excel.ExcelExporter;
+import org.projectforge.excel.ExportColumn;
+import org.projectforge.excel.ExportSheet;
+import org.projectforge.excel.I18nExportColumn;
+import org.projectforge.excel.PropertyMapping;
+import org.projectforge.export.MyExcelExporter;
+
+public class InvoicesExcelExport
+{
+  public InvoicesExcelExport()
+  {
+  }
+
+  public void addDebitorInvoicesSheet(final MyExcelExporter exporter, final String title, final List<RechnungDO> list)
+  {
+    final ExportSheet sheet = exporter.addSheet(title);
+    sheet.createFreezePane(0, 1);
+    final ContentProvider sheetProvider = sheet.getContentProvider();
+
+    final ExportColumn[] cols = new ExportColumn[4];
+    int i = 0;
+    cols[i++] = createColumn(exporter, sheet, RechnungDO.class, "datum");
+    cols[i] = new I18nExportColumn("gross", "fibu.rechnung.bruttoBetrag", 10);
+    exporter.putCurrencyFormat(sheetProvider, cols[i++]);
+
+    cols[i++] = new I18nExportColumn("debitor", "fibu.common.debitor", 60);
+    cols[i++] = new I18nExportColumn("subject", PropUtils.getI18nKey(RechnungDO.class, "betreff"), 100);
+    // column property names
+    sheet.setColumns(cols);
+    final PropertyMapping mapping = new PropertyMapping();
+    for (final RechnungDO entry : list) {
+      entry.recalculate();
+      mapping.add("datum", entry.getDatum());
+      mapping.add("gross", entry.getGrossSum());
+      mapping.add("debitor", entry.getKundeAsString());
+      mapping.add("subject", entry.getBetreff());
+      sheet.addRow(mapping.getMapping(), 0);
+    }
+  }
+
+  public void addCreditorInvoicesSheet(final MyExcelExporter exporter, final String title, final List<EingangsrechnungDO> list)
+  {
+    final ExportSheet sheet = exporter.addSheet(title);
+    sheet.createFreezePane(0, 1);
+    final ContentProvider sheetProvider = sheet.getContentProvider();
+
+    final ExportColumn[] cols = new ExportColumn[4];
+    int i = 0;
+    cols[i++] = createColumn(exporter, sheet, RechnungDO.class, "datum");
+    cols[i] = new I18nExportColumn("gross", "fibu.rechnung.bruttoBetrag", 10);
+    exporter.putCurrencyFormat(sheetProvider, cols[i++]);
+    cols[i++] = new I18nExportColumn("creditor", "fibu.common.creditor", 60);
+    cols[i++] = new I18nExportColumn("subject", PropUtils.getI18nKey(RechnungDO.class, "betreff"), 100);
+    // column property names
+    sheet.setColumns(cols);
+    final PropertyMapping mapping = new PropertyMapping();
+    for (final EingangsrechnungDO entry : list) {
+      entry.recalculate();
+      mapping.add("datum", entry.getDatum());
+      mapping.add("gross", entry.getGrossSum());
+      mapping.add("creditor", entry.getKreditor());
+      mapping.add("subject", entry.getBetreff());
+      sheet.addRow(mapping.getMapping(), 0);
+    }
+  }
+
+  private I18nExportColumn createColumn(final ExcelExporter exporter, final ExportSheet sheet, final Class< ? > clazz, final String property)
+  {
+    final Field field = PropUtils.getField(clazz, property);
+    final PropertyInfo propInfo = PropUtils.get(field);
+    final I18nExportColumn exportColumn = new I18nExportColumn(property, propInfo.i18nKey(), 100);
+    exporter.putFieldFormat(sheet, field, propInfo, exportColumn);
+    return exportColumn;
+  }
+}
