d70047acb7850d65f04ad7ab2a63c9895d4542a8
Kai Reinhard <K.Reinhard@micromata.de>
2013-06-17 08:29:20 +0200

    Recurrence fields are now supported.

--- src/main/java/org/projectforge/plugins/teamcal/rest/TeamEventDOConverter.java
+++ src/main/java/org/projectforge/plugins/teamcal/rest/TeamEventDOConverter.java
@@ -55,13 +55,11 @@ public class TeamEventDOConverter
     event.setNote(src.getNote());
     event.setSubject(src.getSubject());
     if (src instanceof TeamEventDO) {
-      event.setCalendarId(((TeamEventDO) src).getCalendarId());
-      setReminderSettings(event, (TeamEventDO) src);
+      copyFields(event, (TeamEventDO) src);
     } else if (src instanceof TeamRecurrenceEvent) {
       final TeamEventDO master = ((TeamRecurrenceEvent) src).getMaster();
       if (master != null) {
-        event.setCalendarId(master.getCalendarId());
-        setReminderSettings(event, master);
+        copyFields(event, master);
       }
     }
     return event;
@@ -73,39 +71,41 @@ public class TeamEventDOConverter
       return null;
     }
     final CalendarEventObject event = new CalendarEventObject();
-    DOConverter.copyFields(event, src);
     event.setUid(src.getUid());
     event.setStartDate(src.getStartDate());
     event.setEndDate(src.getEndDate());
     event.setLocation(src.getLocation());
     event.setNote(src.getNote());
     event.setSubject(src.getSubject());
-    event.setCalendarId(src.getCalendarId());
-    setReminderSettings(event, src);
+    copyFields(event, src);
     return event;
   }
 
-  private static void setReminderSettings(final CalendarEventObject event, final TeamEventDO src)
+  private static void copyFields(final CalendarEventObject event, final TeamEventDO src)
   {
-    if (src.getReminderActionType() == null || src.getReminderDuration() == null || src.getReminderDurationUnit() == null) {
-      return;
-    }
-    event.setReminderType(src.getReminderActionType().toString());
-    event.setReminderDuration(src.getReminderDuration());
-    final ReminderDurationUnit unit = src.getReminderDurationUnit();
-    event.setReminderUnit(unit.toString());
-    final DateHolder date = new DateHolder(src.getStartDate());
-    if (unit == ReminderDurationUnit.MINUTES) {
-      date.add(Calendar.MINUTE, -src.getReminderDuration());
-      event.setReminder(date.getDate());
-    } else if (unit == ReminderDurationUnit.HOURS) {
-      date.add(Calendar.HOUR, -src.getReminderDuration());
-      event.setReminder(date.getDate());
-    } else if (unit == ReminderDurationUnit.DAYS) {
-      date.add(Calendar.DAY_OF_YEAR, -src.getReminderDuration());
-      event.setReminder(date.getDate());
-    } else {
-      log.warn("ReminderDurationUnit '" + src.getReminderDurationUnit() + "' not yet implemented.");
+    event.setCalendarId(src.getCalendarId());
+    event.setRecurrenceRule(src.getRecurrenceRule());
+    event.setRecurrenceExDate(src.getRecurrenceExDate());
+    event.setRecurrenceUntil(src.getRecurrenceUntil());
+    DOConverter.copyFields(event, src);
+    if (src.getReminderActionType() != null && src.getReminderDuration() != null && src.getReminderDurationUnit() != null) {
+      event.setReminderType(src.getReminderActionType().toString());
+      event.setReminderDuration(src.getReminderDuration());
+      final ReminderDurationUnit unit = src.getReminderDurationUnit();
+      event.setReminderUnit(unit.toString());
+      final DateHolder date = new DateHolder(src.getStartDate());
+      if (unit == ReminderDurationUnit.MINUTES) {
+        date.add(Calendar.MINUTE, -src.getReminderDuration());
+        event.setReminder(date.getDate());
+      } else if (unit == ReminderDurationUnit.HOURS) {
+        date.add(Calendar.HOUR, -src.getReminderDuration());
+        event.setReminder(date.getDate());
+      } else if (unit == ReminderDurationUnit.DAYS) {
+        date.add(Calendar.DAY_OF_YEAR, -src.getReminderDuration());
+        event.setReminder(date.getDate());
+      } else {
+        log.warn("ReminderDurationUnit '" + src.getReminderDurationUnit() + "' not yet implemented.");
+      }
     }
   }
 }
