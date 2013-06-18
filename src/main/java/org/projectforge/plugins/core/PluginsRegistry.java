f143ac417631763afd36ca13ddd0b1d0e597611f
bduong <b.duong@micromata.de>
2013-06-18 15:43:54 +0200

    Removed plugin entry for skill-matrix.

--- src/main/java/org/projectforge/plugins/core/PluginsRegistry.java
+++ src/main/java/org/projectforge/plugins/core/PluginsRegistry.java
@@ -32,7 +32,6 @@ import org.projectforge.continuousdb.UpdateEntry;
 import org.projectforge.core.ConfigXml;
 import org.projectforge.core.CronSetup;
 import org.projectforge.plugins.memo.MemoPlugin;
-import org.projectforge.plugins.skillmatrix.SkillMatrixPlugin;
 import org.projectforge.plugins.teamcal.TeamCalPlugin;
 import org.projectforge.plugins.todo.ToDoPlugin;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
@@ -43,122 +42,122 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
  */
 public class PluginsRegistry
 {
-	private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(PluginsRegistry.class);
+  private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(PluginsRegistry.class);
 
-	/**
-	 * Defines some built-in plugins. Don't forget to add the pluginContext.xml files to TestConfiguration.
-	 */
-	private final AbstractPlugin[] builtinPlugins = new AbstractPlugin[] { new ToDoPlugin(), new MemoPlugin(), new TeamCalPlugin(), new SkillMatrixPlugin()};
+  /**
+   * Defines some built-in plugins. Don't forget to add the pluginContext.xml files to TestConfiguration.
+   */
+  private final AbstractPlugin[] builtinPlugins = new AbstractPlugin[] { new ToDoPlugin(), new MemoPlugin(), new TeamCalPlugin()};
 
-	private static PluginsRegistry instance;
+  private static PluginsRegistry instance;
 
-	private IResourceSettings resourceSettings;
+  private IResourceSettings resourceSettings;
 
-	private ConfigurableListableBeanFactory beanFactory;
+  private ConfigurableListableBeanFactory beanFactory;
 
-	private final List<AbstractPlugin> plugins = new ArrayList<AbstractPlugin>();
+  private final List<AbstractPlugin> plugins = new ArrayList<AbstractPlugin>();
 
-	public synchronized static PluginsRegistry instance()
-	{
-		if (instance == null) {
-			instance = new PluginsRegistry();
-		}
-		return instance;
-	}
+  public synchronized static PluginsRegistry instance()
+  {
+    if (instance == null) {
+      instance = new PluginsRegistry();
+    }
+    return instance;
+  }
 
-	public void register(final AbstractPlugin plugin)
-	{
-		for (final AbstractPlugin pl : plugins) {
-			if (pl.getClass().equals(plugin.getClass()) == true) {
-				log.warn("Can't add plugin twice. Plugin '" + plugin.getClass() + "' already added.");
-				return;
-			}
-		}
-		plugins.add(plugin);
-	}
+  public void register(final AbstractPlugin plugin)
+  {
+    for (final AbstractPlugin pl : plugins) {
+      if (pl.getClass().equals(plugin.getClass()) == true) {
+        log.warn("Can't add plugin twice. Plugin '" + plugin.getClass() + "' already added.");
+        return;
+      }
+    }
+    plugins.add(plugin);
+  }
 
-	public List<AbstractPlugin> getPlugins()
-	{
-		return plugins;
-	}
+  public List<AbstractPlugin> getPlugins()
+  {
+    return plugins;
+  }
 
-	public PluginsRegistry()
-	{
-	}
+  public PluginsRegistry()
+  {
+  }
 
-	public void set(final SystemUpdater systemUpdater)
-	{
-		for (final AbstractPlugin plugin : plugins) {
-			final UpdateEntry updateEntry = plugin.getInitializationUpdateEntry();
-			if (updateEntry != null) {
-				if (updateEntry.isInitial() == false) {
-					log.error("The given UpdateEntry returned by plugin.getInitializationUpdateEntry() is not initial! Please use constructor without parameter version: "
-							+ plugin.getClass());
-				}
-				systemUpdater.register(updateEntry);
-			}
-			final List<UpdateEntry> updateEntries = plugin.getUpdateEntries();
-			if (updateEntries != null) {
-				for (final UpdateEntry entry : updateEntries) {
-					if (entry.isInitial() == true) {
-						log.error("The given UpdateEntry returned by plugin.getUpdateEntries() is initial! Please use constructor with parameter version: "
-								+ plugin.getClass() + ": " + entry.getDescription());
-					}
-				}
-				systemUpdater.register(updateEntries);
-			}
-		}
-	}
+  public void set(final SystemUpdater systemUpdater)
+  {
+    for (final AbstractPlugin plugin : plugins) {
+      final UpdateEntry updateEntry = plugin.getInitializationUpdateEntry();
+      if (updateEntry != null) {
+        if (updateEntry.isInitial() == false) {
+          log.error("The given UpdateEntry returned by plugin.getInitializationUpdateEntry() is not initial! Please use constructor without parameter version: "
+              + plugin.getClass());
+        }
+        systemUpdater.register(updateEntry);
+      }
+      final List<UpdateEntry> updateEntries = plugin.getUpdateEntries();
+      if (updateEntries != null) {
+        for (final UpdateEntry entry : updateEntries) {
+          if (entry.isInitial() == true) {
+            log.error("The given UpdateEntry returned by plugin.getUpdateEntries() is initial! Please use constructor with parameter version: "
+                + plugin.getClass() + ": " + entry.getDescription());
+          }
+        }
+        systemUpdater.register(updateEntries);
+      }
+    }
+  }
 
-	public void set(final ConfigurableListableBeanFactory beanFactory, final IResourceSettings resourceSettings)
-	{
-		this.beanFactory = beanFactory;
-		this.resourceSettings = resourceSettings;
-	}
+  public void set(final ConfigurableListableBeanFactory beanFactory, final IResourceSettings resourceSettings)
+  {
+    this.beanFactory = beanFactory;
+    this.resourceSettings = resourceSettings;
+  }
 
-	/**
-	 * Load built-in plugins and plugins which are configured in config.xml.
-	 */
-	public void loadPlugins()
-	{
-		for (final AbstractPlugin plugin : builtinPlugins) {
-			register(plugin);
-		}
-		final ConfigXml xmlConfiguration = ConfigXml.getInstance();
-		final String[] pluginMainClasses = xmlConfiguration.getPluginMainClasses();
-		if (pluginMainClasses != null) {
-			for (final String pluginMainClassName : pluginMainClasses) {
-				try {
-					final Class< ? > pluginMainClass = Class.forName(pluginMainClassName);
-					try {
-						final AbstractPlugin plugin = (AbstractPlugin) pluginMainClass.newInstance();
-						register(plugin);
-					} catch (final ClassCastException ex) {
-						log.error("Couldn't load plugin, class '" + pluginMainClassName + "' isn't of type AbstractPlugin.");
-					} catch (final InstantiationException ex) {
-						log.error("Couldn't load plugin, class '" + pluginMainClassName + "' can't be instantiated: " + ex);
-					} catch (final IllegalAccessException ex) {
-						log.error("Couldn't load plugin, class '" + pluginMainClassName + "' can't be instantiated: " + ex);
-					}
-				} catch (final ClassNotFoundException ex) {
-					log.error("Couldn't load plugin, class '" + pluginMainClassName + "' not found");
-				}
-			}
-		}
-	}
+  /**
+   * Load built-in plugins and plugins which are configured in config.xml.
+   */
+  public void loadPlugins()
+  {
+    for (final AbstractPlugin plugin : builtinPlugins) {
+      register(plugin);
+    }
+    final ConfigXml xmlConfiguration = ConfigXml.getInstance();
+    final String[] pluginMainClasses = xmlConfiguration.getPluginMainClasses();
+    if (pluginMainClasses != null) {
+      for (final String pluginMainClassName : pluginMainClasses) {
+        try {
+          final Class< ? > pluginMainClass = Class.forName(pluginMainClassName);
+          try {
+            final AbstractPlugin plugin = (AbstractPlugin) pluginMainClass.newInstance();
+            register(plugin);
+          } catch (final ClassCastException ex) {
+            log.error("Couldn't load plugin, class '" + pluginMainClassName + "' isn't of type AbstractPlugin.");
+          } catch (final InstantiationException ex) {
+            log.error("Couldn't load plugin, class '" + pluginMainClassName + "' can't be instantiated: " + ex);
+          } catch (final IllegalAccessException ex) {
+            log.error("Couldn't load plugin, class '" + pluginMainClassName + "' can't be instantiated: " + ex);
+          }
+        } catch (final ClassNotFoundException ex) {
+          log.error("Couldn't load plugin, class '" + pluginMainClassName + "' not found");
+        }
+      }
+    }
+  }
 
-	public void initialize()
-	{
-		for (final AbstractPlugin plugin : plugins) {
-			plugin.setResourceSettings(resourceSettings);
-			beanFactory.autowireBeanProperties(plugin, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
-			plugin.init();
-		}
-	}
+  public void initialize()
+  {
+    for (final AbstractPlugin plugin : plugins) {
+      plugin.setResourceSettings(resourceSettings);
+      beanFactory.autowireBeanProperties(plugin, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
+      plugin.init();
+    }
+  }
 
-	public void registerCronJobs(final CronSetup cronSetup) {
-		for (final AbstractPlugin plugin : plugins) {
-			plugin.registerCronJob(cronSetup);
-		}
-	}
+  public void registerCronJobs(final CronSetup cronSetup) {
+    for (final AbstractPlugin plugin : plugins) {
+      plugin.registerCronJob(cronSetup);
+    }
+  }
 }
