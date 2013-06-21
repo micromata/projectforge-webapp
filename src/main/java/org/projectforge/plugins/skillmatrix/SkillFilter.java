e269b0e1541cf1483a60a829c19f34d4b458f52f
bduong <b.duong@micromata.de>
2013-06-21 14:46:08 +0200

    Added SkillFilter.

---
+++ src/main/java/org/projectforge/plugins/skillmatrix/SkillFilter.java
@@ -0,0 +1,34 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project   ProjectForge
+//
+// Copyright 2001-2009, Micromata GmbH, Kai Reinhard
+//           All rights reserved.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.plugins.skillmatrix;
+
+import org.projectforge.core.BaseSearchFilter;
+
+/**
+ * @author Billy Duong (b.duong@micromata.de)
+ *
+ */
+public class SkillFilter extends BaseSearchFilter
+{
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -8140231682294892497L;
+
+  public SkillFilter() {
+  }
+
+  public SkillFilter(final SkillFilter baseSearchFilter)
+  {
+    super(baseSearchFilter);
+  }
+
+}
