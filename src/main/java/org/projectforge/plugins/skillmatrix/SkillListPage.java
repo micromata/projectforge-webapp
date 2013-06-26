79be7ce3186a4b33e067b0081ff7310771348bb4
bduong <b.duong@micromata.de>
2013-06-26 14:07:11 +0200

    Changed variable name.

--- src/main/java/org/projectforge/plugins/skillmatrix/SkillListPage.java
+++ src/main/java/org/projectforge/plugins/skillmatrix/SkillListPage.java
@@ -105,7 +105,7 @@ public class SkillListPage extends AbstractListPage<SkillListForm, SkillDao, Ski
       }
     };
 
-    final CellItemListenerPropertyColumn<SkillDO> modified = new CellItemListenerPropertyColumn<SkillDO>(getString("modified"),
+    final CellItemListenerPropertyColumn<SkillDO> lastUpdate = new CellItemListenerPropertyColumn<SkillDO>(getString("lastUpdate"),
         getSortable("lastUpdate", sortable), "lastUpdate", cellItemListener);
 
     final CellItemListenerPropertyColumn<SkillDO> title = new CellItemListenerPropertyColumn<SkillDO>(SkillDO.class, getSortable("title",
@@ -130,7 +130,7 @@ public class SkillListPage extends AbstractListPage<SkillListForm, SkillDao, Ski
     columns.add(comment);
     columns.add(rateable);
     columns.add(created);
-    columns.add(modified);
+    columns.add(lastUpdate);
 
     return columns;
   }
