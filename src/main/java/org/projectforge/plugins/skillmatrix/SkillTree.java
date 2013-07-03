988b17c2003915abdaf75c77276ed2baf0b7af27
bduong <b.duong@micromata.de>
2013-07-03 10:46:16 +0200

    Refactoring.

--- src/main/java/org/projectforge/plugins/skillmatrix/SkillTree.java
+++ src/main/java/org/projectforge/plugins/skillmatrix/SkillTree.java
@@ -274,12 +274,7 @@ public class SkillTree extends AbstractCache implements Serializable
 
     if (newRoot == null) {
       log.fatal("OUPS, no skill found (ProjectForge database not initialized?) OK, initialize it ...");
-      final SkillDO rootSkill = new SkillDO();
-      rootSkill.setTitle("root");
-      rootSkill.setDescription("ProjectForge root task");
-      skillDao.internalSave(rootSkill);
-      newRoot = new SkillNode();
-      newRoot.setSkill(rootSkill);
+      newRoot = createRootNode();
       skillMap.put(newRoot.getId(), newRoot);
     }
     this.root = newRoot;
@@ -341,5 +336,19 @@ public class SkillTree extends AbstractCache implements Serializable
   {
     this.skillDao = skillDao;
     return this;
+  }
+
+  /**
+   * Creates a root node for the skill tree and saves it in the database.
+   * @return a "dummy" skill node.
+   */
+  private SkillNode createRootNode() {
+    final SkillNode newRoot = new SkillNode();
+    final SkillDO rootSkill = new SkillDO();
+    rootSkill.setTitle("root");
+    rootSkill.setDescription("ProjectForge root task");
+    skillDao.internalSave(rootSkill);
+    newRoot.setSkill(rootSkill);
+    return newRoot;
   }
 }
