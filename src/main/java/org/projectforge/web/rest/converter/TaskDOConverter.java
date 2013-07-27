d9b4596ae5ef059436934cf89951c9b16d17b9e9
Kai Reinhard <K.Reinhard@micromata.de>
2013-07-27 23:32:10 +0200

    Nothing real (copy right header).

--- src/main/java/org/projectforge/web/rest/converter/TaskDOConverter.java
+++ src/main/java/org/projectforge/web/rest/converter/TaskDOConverter.java
@@ -7,17 +7,17 @@
 //
 // ProjectForge is dual-licensed.
 //
-// task community edition is free software; you can redistribute it and/or
+// This community edition is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as published
 // by the Free Software Foundation; version 3 of the License.
 //
-// task community edition is distributed in the hope that it will be useful,
+// This community edition is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
 // Public License for more details.
 //
 // You should have received a copy of the GNU General Public License along
-// with task program; if not, see http://www.gnu.org/licenses/.
+// with this program; if not, see http://www.gnu.org/licenses/.
 //
 /////////////////////////////////////////////////////////////////////////////
 
