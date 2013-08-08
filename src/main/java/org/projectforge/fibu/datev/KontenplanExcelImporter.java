33dad1ac55b1c8953c2ae6f26865a7bb77ea8f11
Kai Reinhard <K.Reinhard@micromata.de>
2013-08-08 23:43:05 +0200

    Number of head row modified.

--- src/main/java/org/projectforge/fibu/datev/KontenplanExcelImporter.java
+++ src/main/java/org/projectforge/fibu/datev/KontenplanExcelImporter.java
@@ -43,17 +43,17 @@ public class KontenplanExcelImporter
   private static final Logger log = Logger.getLogger(KontenplanExcelImporter.class);
 
   /** In dieser Zeile stehen die Überschriften der Spalten für die Konten. */
-  public static final int ROW_COLUMNNAMES = 2;
-  
+  public static final int ROW_COLUMNNAMES = 1;
+
   public void doImport(final ImportStorage<KontoDO> storage, final InputStream is, final ActionLog actionLog) throws Exception
   {
-    ExcelImport<KontenplanExcelRow> imp = new ExcelImport<KontenplanExcelRow>(is);
+    final ExcelImport<KontenplanExcelRow> imp = new ExcelImport<KontenplanExcelRow>(is);
     for (short idx = 0; idx < imp.getWorkbook().getNumberOfSheets(); idx++) {
       imp.setActiveSheet(idx);
-      String name = imp.getWorkbook().getSheetName(idx);
+      final String name = imp.getWorkbook().getSheetName(idx);
       if (NAME_OF_EXCEL_SHEET.equals(name)) {
         imp.setActiveSheet(idx);
-        HSSFSheet sheet = imp.getWorkbook().getSheetAt(idx);
+        final HSSFSheet sheet = imp.getWorkbook().getSheetAt(idx);
         importKontenplan(storage, imp, sheet, actionLog);
         return;
       }
@@ -63,35 +63,36 @@ public class KontenplanExcelImporter
 
   private void importKontenplan(final ImportStorage<KontoDO> storage, final ExcelImport<KontenplanExcelRow> imp, final HSSFSheet sheet,
       final ActionLog actionLog) throws Exception
-  {
-    ImportedSheet<KontoDO> importedSheet = new ImportedSheet<KontoDO>();
+      {
+    final ImportedSheet<KontoDO> importedSheet = new ImportedSheet<KontoDO>();
     storage.addSheet(importedSheet);
     importedSheet.setName(NAME_OF_EXCEL_SHEET);
     imp.setNameRowIndex(ROW_COLUMNNAMES);
     imp.setStartingRowIndex(ROW_COLUMNNAMES + 1);
     imp.setRowClass(KontenplanExcelRow.class);
 
-    Map<String, String> map = new HashMap<String, String>();
+    final Map<String, String> map = new HashMap<String, String>();
     map.put("Konto", "konto");
     map.put("Bezeichnung", "bezeichnung");
+    map.put("Beschriftung", "bezeichnung");
     imp.setColumnMapping(map);
 
     KontenplanExcelRow[] rows = new KontenplanExcelRow[0];
     rows = imp.convertToRows(KontenplanExcelRow.class);
     for (int i = 0; i < rows.length; i++) {
       actionLog.incrementCounterSuccess();
-      KontoDO konto = convertKonto(rows[i]);
-      ImportedElement<KontoDO> element = new ImportedElement<KontoDO>(storage.nextVal(), KontoDO.class,
+      final KontoDO konto = convertKonto(rows[i]);
+      final ImportedElement<KontoDO> element = new ImportedElement<KontoDO>(storage.nextVal(), KontoDO.class,
           DatevImportDao.KONTO_DIFF_PROPERTIES);
       element.setValue(konto);
       importedSheet.addElement(element);
       log.debug(konto);
     }
-  }
+      }
 
-  private KontoDO convertKonto(KontenplanExcelRow row) throws Exception
+  private KontoDO convertKonto(final KontenplanExcelRow row) throws Exception
   {
-    KontoDO konto = new KontoDO();
+    final KontoDO konto = new KontoDO();
     konto.setNummer(row.konto);
     konto.setBezeichnung(row.bezeichnung);
     return konto;
