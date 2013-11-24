75957b54b28c113e71dfbf357a033fe6f835e74d
Kai Reinhard <K.Reinhard@micromata.de>
2013-11-24 09:14:17 +0100

    PFUserDO.lastPasswordChange added.

--- src/main/java/org/projectforge/database/DatabaseCoreUpdates.java
+++ src/main/java/org/projectforge/database/DatabaseCoreUpdates.java
@@ -71,13 +71,15 @@ public class DatabaseCoreUpdates
     // 5.3
     // /////////////////////////////////////////////////////////////////
     list.add(new UpdateEntryImpl(CORE_REGION_ID, "5.3", "2013-10-20",
-        "Adds t_tenant, tenant_id to all entities for multi-tenancy. Adds t_configuration.is_global.") {
+        "Adds t_tenant, tenant_id to all entities for multi-tenancy. Adds t_configuration.is_global and t_pf_user.last_password_change.") {
       final Table configurationTable = new Table(ConfigurationDO.class);
 
       @Override
       public UpdatePreCheckStatus runPreCheck()
       {
-        if (dao.doEntitiesExist(TenantDO.class) == false || dao.doTableAttributesExist(configurationTable, "global") == false) {
+        if (dao.doEntitiesExist(TenantDO.class) == false
+            || dao.doTableAttributesExist(configurationTable, "global") == false
+            || dao.doTableAttributesExist(PFUserDO.class, "lastPasswordChange") == false) {
           return UpdatePreCheckStatus.READY_FOR_UPDATE;
         }
         final List<RegistryEntry> list = Registry.instance().getOrderedList();
@@ -103,6 +105,9 @@ public class DatabaseCoreUpdates
       {
         if (dao.doTableAttributesExist(configurationTable, "global") == false) {
           dao.addTableAttributes(configurationTable, new TableAttribute(ConfigurationDO.class, "global").setDefaultValue("false"));
+        }
+        if (dao.doTableAttributesExist(PFUserDO.class, "lastPasswordChange") == false) {
+          dao.addTableAttributes(PFUserDO.class, "lastPasswordChange");
         }
 
         if (dao.doEntitiesExist(TenantDO.class) == false) {
