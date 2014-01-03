bc2a1f727143779bacaf8d6d71eb0e6bb51551c2
Kai Reinhard <K.Reinhard@micromata.de>
2014-01-03 09:38:23 +0100

    Initial revision.

---
+++ src/main/java/org/projectforge/multitenancy/TenantRegistry.java
@@ -0,0 +1,114 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2014 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.multitenancy;
+
+import org.apache.log4j.Logger;
+import org.projectforge.access.AccessDao;
+import org.projectforge.fibu.AuftragDao;
+import org.projectforge.fibu.ProjektDao;
+import org.projectforge.registry.Registry;
+import org.projectforge.task.TaskDao;
+import org.projectforge.task.TaskTree;
+
+/**
+ * Holds caches of a single tenant. After the configured time to live (TTL) this registry is detached from {@link TenantRegistryMap}.
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ */
+public class TenantRegistry
+{
+  /**
+   * Default time to live for an holder.
+   */
+  private static final long TIME_TO_LIVE_MS = 60 * 60 * 1000;
+
+  private static Logger log = Logger.getLogger(TenantRegistry.class);
+
+  private long lastUsage;
+
+  private final long timeToLive;
+
+  private TaskTree taskTree;
+
+  private final TenantDO tenant;
+
+  public TenantRegistry(final TenantDO tenant)
+  {
+    this.tenant = tenant;
+    this.lastUsage = System.currentTimeMillis();
+    this.timeToLive = TIME_TO_LIVE_MS;
+    final TaskTree taskTree = new TaskTree();
+    final Registry registry = Registry.instance();
+    taskTree.setAccessDao(registry.getDao(AccessDao.class));
+    taskTree.setAuftragDao(registry.getDao(AuftragDao.class));
+    taskTree.setKostCache(registry.getKostCache());
+    taskTree.setProjektDao(registry.getDao(ProjektDao.class));
+    taskTree.setTaskDao(registry.getDao(TaskDao.class));
+  }
+
+  /**
+   * @return true if the last usage of this holder more ms in the past than the TTL.
+   */
+  public boolean isOutdated()
+  {
+    return System.currentTimeMillis() - lastUsage > 0;
+  }
+
+  /**
+   * @return the taskTree
+   */
+  public TaskTree getTaskTree()
+  {
+    updateUsageTime();
+    return taskTree;
+  }
+
+  /**
+   * @return the tenant
+   */
+  public TenantDO getTenant()
+  {
+    return tenant;
+  }
+
+  private void updateUsageTime()
+  {
+    this.lastUsage = System.currentTimeMillis();
+  }
+
+  /**
+   * @return the lastUsage time in ms.
+   */
+  public long getLastUsage()
+  {
+    return lastUsage;
+  }
+
+  /**
+   * @return the timeToLive of this holder in ms.
+   */
+  public long getTimeToLive()
+  {
+    return timeToLive;
+  }
+}
---
+++ src/main/java/org/projectforge/multitenancy/TenantRegistryMap.java
@@ -0,0 +1,94 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2014 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.multitenancy;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.projectforge.common.AbstractCache;
+
+/**
+ * Holds TenantCachesHolder element and detaches them if not used for some time to save memory.
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ */
+public class TenantRegistryMap extends AbstractCache
+{
+  private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(TenantRegistryMap.class);
+
+  private static final long EXPIRE_TIME = AbstractCache.TICKS_PER_DAY;
+
+  private final Map<Integer, TenantRegistry> tenantRegistryMap = new HashMap<Integer, TenantRegistry>();
+
+  private static TenantRegistryMap instance = new TenantRegistryMap();
+
+  public static TenantRegistryMap getInstance()
+  {
+    return instance;
+  }
+
+  private TenantRegistryMap()
+  {
+    super(EXPIRE_TIME);
+  }
+
+  public TenantRegistry getTenantRegistry()
+  {
+    checkRefresh();
+    final TenantDO tenant = TenantChecker.getCurrentTenant();
+    if (tenant == null) {
+      log.warn("Current tenant is null in ThreadLocal.");
+      return null;
+    }
+    TenantRegistry registry = tenantRegistryMap.get(tenant.getId());
+    if (registry == null) {
+      registry = new TenantRegistry(tenant);
+      tenantRegistryMap.put(tenant.getId(), registry);
+    }
+    return registry;
+  }
+
+  /**
+   * @see org.projectforge.common.AbstractCache#refresh()
+   */
+  @Override
+  protected void refresh()
+  {
+    log.info("Refreshing " + TenantRegistry.class.getName() + "...");
+    final Iterator<Map.Entry<Integer, TenantRegistry>> it = tenantRegistryMap.entrySet().iterator();
+    while (it.hasNext() == true) {
+      final Map.Entry<Integer, TenantRegistry> entry = it.next();
+      final TenantRegistry registry = entry.getValue();
+      if (registry.isOutdated() == true) {
+        final TenantDO tenant = registry.getTenant();
+        log.info("Detaching caches of tenant '"
+            + (tenant != null ? tenant.getShortName() : "null")
+            + "' with id "
+            + (tenant != null ? tenant.getId() : "null"));
+        it.remove();
+      }
+    }
+    log.info("Refreshing of " + TenantRegistry.class.getName() + " done.");
+  }
+}
