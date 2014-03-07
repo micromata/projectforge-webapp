66204a485b9e0b055388d15b92a9e602d607e2a3
Werner Feder <werner.feder@t-online.de>
2014-03-07 18:04:26 +0100

    Added training and invitee stuff.

--- src/main/java/org/projectforge/plugins/skillmatrix/SkillMatrixPlugin.java
+++ src/main/java/org/projectforge/plugins/skillmatrix/SkillMatrixPlugin.java
@@ -35,14 +35,20 @@ import org.projectforge.web.MenuItemDefId;
 public class SkillMatrixPlugin extends AbstractPlugin
 {
   public static final String ID_SKILL_RATING = "skillRating";
+
   public static final String ID_SKILL = "skill";
+
   public static final String ID_SKILL_TREE = "skillTree";
+
+  public static final String ID_SKILL_TRAINING = "skillTraining";
+
+  public static final String ID_SKILL_TRAINING_INVITEE = "skillTrainingInvitee";
 
   public static final String RESOURCE_BUNDLE_NAME = SkillMatrixPlugin.class.getPackage().getName() + ".SkillMatrixI18nResources";
 
   static UserPrefArea USER_PREF_AREA;
 
-  private static final Class< ? >[] PERSISTENT_ENTITIES = new Class< ? >[] { SkillDO.class, SkillRatingDO.class };
+  private static final Class< ? >[] PERSISTENT_ENTITIES = new Class< ? >[] { SkillDO.class, SkillRatingDO.class, TrainingDO.class, InviteeDO.class};
 
   public static final String I18N_KEY_SKILLMATRIX_PREFIX = "plugins.skillmatrix";
 
@@ -52,12 +58,20 @@ public class SkillMatrixPlugin extends AbstractPlugin
 
   public static final String I18N_KEY_SKILLTREE_MENU_ENTRY = "plugins.skillmatrix.skilltree.menu";
 
+  public static final String I18N_KEY_SKILLTRAINING_MENU_ENTRY = "plugins.skillmatrix.skilltraining.menu";
+
+  public static final String I18N_KEY_SKILLTRAINING_INVITEE_MENU_ENTRY = "plugins.skillmatrix.skilltraining.invitee.menu";
+
+  // public static final String I18N_KEY_SKILLTRAININGEDIT_MENU_ENTRY = "plugins.skillmatrix.skilltraining.edit.menu";
+
   /**
    * This dao should be defined in pluginContext.xml (as resources) for proper initialization.
    */
   private SkillDao skillDao;
-
   private SkillRatingDao skillRatingDao;
+  private TrainingDao trainingDao;
+  private InviteeDao inviteeDao;
+
 
   @Override
   public Class< ? >[] getPersistentEntities()
@@ -75,10 +89,14 @@ public class SkillMatrixPlugin extends AbstractPlugin
     SkillMatrixPluginUpdates.dao = getDatabaseUpdateDao();
     register(ID_SKILL_RATING, SkillRatingDao.class, skillRatingDao, I18N_KEY_SKILLMATRIX_PREFIX);
     register(ID_SKILL, SkillDao.class, skillDao, I18N_KEY_SKILLMATRIX_PREFIX);
+    register(ID_SKILL_TRAINING, TrainingDao.class, trainingDao, I18N_KEY_SKILLMATRIX_PREFIX);
+    register(ID_SKILL_TRAINING_INVITEE, InviteeDao.class, inviteeDao, I18N_KEY_SKILLMATRIX_PREFIX);
 
     // Register the web part:
     registerWeb(ID_SKILL_RATING, SkillRatingListPage.class, SkillRatingEditPage.class);
     registerWeb(ID_SKILL, SkillListPage.class, SkillEditPage.class);
+    registerWeb(ID_SKILL_TRAINING, TrainingListPage.class, TrainingEditPage.class);
+    registerWeb(ID_SKILL_TRAINING_INVITEE, InviteeListPage.class, InviteeEditPage.class);
 
     // Register the menu entry as sub menu entry of the misc menu:
     final MenuItemDef parentMenu = getMenuItemDef(MenuItemDefId.MISC);
@@ -86,12 +104,16 @@ public class SkillMatrixPlugin extends AbstractPlugin
     registerMenuItem(new MenuItemDef(parentMenu, ID_SKILL_TREE, 5, I18N_KEY_SKILLTREE_MENU_ENTRY, SkillTreePage.class));
     registerMenuItem(new MenuItemDef(parentMenu, ID_SKILL_RATING, 5, I18N_KEY_SKILLRATING_MENU_ENTRY, SkillRatingListPage.class));
     registerMenuItem(new MenuItemDef(parentMenu, ID_SKILL, 5, I18N_KEY_SKILL_MENU_ENTRY, SkillListPage.class));
+    registerMenuItem(new MenuItemDef(parentMenu, ID_SKILL_TRAINING, 5, I18N_KEY_SKILLTRAINING_MENU_ENTRY, TrainingListPage.class));
+    registerMenuItem(new MenuItemDef(parentMenu, ID_SKILL_TRAINING_INVITEE, 5, I18N_KEY_SKILLTRAINING_INVITEE_MENU_ENTRY, InviteeListPage.class));
 
     // .setMobileMenu(SkillRatingMobileListPage.class, 10));
 
     // Define the access management:
     registerRight(new SkillRight());
     registerRight(new SkillRatingRight());
+    registerRight(new TrainingRight());
+    registerRight(new InviteeRight());
 
     // All the i18n stuff:
     addResourceBundle(RESOURCE_BUNDLE_NAME);
@@ -109,6 +131,16 @@ public class SkillMatrixPlugin extends AbstractPlugin
   public void setSkillRatingDao(final SkillRatingDao skillRatingDao)
   {
     this.skillRatingDao = skillRatingDao;
+  }
+
+  public void setTrainingDao(final TrainingDao trainingDao)
+  {
+    this.trainingDao = trainingDao;
+  }
+
+  public void setInviteeDao(final InviteeDao inviteeDao)
+  {
+    this.inviteeDao = inviteeDao;
   }
 
   /**
