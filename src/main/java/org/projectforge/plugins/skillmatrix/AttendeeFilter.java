0510e7404703766ad426e7efc2c39e0844a94c7c
Werner Feder <werner.feder@t-online.de>
2014-03-20 17:23:49 +0100

    Implement required methods.

--- src/main/java/org/projectforge/plugins/skillmatrix/AttendeeFilter.java
+++ src/main/java/org/projectforge/plugins/skillmatrix/AttendeeFilter.java
@@ -18,4 +18,62 @@ import org.projectforge.core.BaseSearchFilter;
 public class AttendeeFilter extends BaseSearchFilter
 {
   private static final long serialVersionUID = 1278054558397436842L;
+
+  private Integer attendeeId, skillId, trainingId;
+
+  /**
+   * @return the attendeeId
+   */
+  public Integer getAttendeeId()
+  {
+    return attendeeId;
+  }
+
+  /**
+   * @param attendeeId the attendeeId to set
+   * @return this for chaining.
+   */
+  public AttendeeFilter setAttendeeId(final Integer attendeeId)
+  {
+    this.attendeeId = attendeeId;
+    return this;
+  }
+
+  /**
+   * @return the skillId
+   */
+  public Integer getSkillId()
+  {
+    return skillId;
+  }
+
+  /**
+   * @param skillId the skillId to set
+   * @return this for chaining.
+   */
+  public AttendeeFilter setSkillId(final Integer skillId)
+  {
+    this.skillId = skillId;
+    return this;
+  }
+
+  /**
+   * @return the trainingId
+   */
+  public Integer getTrainingId()
+  {
+    return trainingId;
+  }
+
+  /**
+   * @param trainingId the trainingId to set
+   * @return this for chaining.
+   */
+  public AttendeeFilter setTrainingId(final Integer trainingId)
+  {
+    this.trainingId = trainingId;
+    return this;
+  }
+
+
 }
