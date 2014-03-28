d9df29a6aca644d86e16b690d75eca165b08b10a
Werner Feder <werner.feder@t-online.de>
2014-03-28 15:45:56 +0100

    root node initialized.

--- src/main/java/org/projectforge/plugins/skillmatrix/SkillTree.java
+++ src/main/java/org/projectforge/plugins/skillmatrix/SkillTree.java
@@ -348,6 +348,11 @@ public class SkillTree extends AbstractCache implements Serializable
     rootSkill.setTitle("root");
     rootSkill.setDescription("ProjectForge root skill");
     rootSkill.setRateable(false);
+
+    rootSkill.setFullAccessGroupIds("4");
+    rootSkill.setReadOnlyAccessGroupIds("4");
+    rootSkill.setTrainingAccessGroupIds("4");
+
     // TODO internalSave gives a no hibernate session bound to thread warning, this workaround should probably exchanged for a better solution
     // skillDao.internalSave(rootSkill);
     skillDao.getHibernateTemplate().save(rootSkill);
