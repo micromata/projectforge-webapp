fba10d8514865d51fe4aadbeda09f1aab882b862
Werner Feder <werner.feder@t-online.de>
2014-04-04 16:47:11 +0200

    Initial revision.

---
+++ src/main/java/org/projectforge/addresses/AddressFilter.java
@@ -0,0 +1,31 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project   ProjectForge
+//
+// Copyright 2001-2009, Micromata GmbH, Kai Reinhard
+//           All rights reserved.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.addresses;
+
+import org.projectforge.core.BaseSearchFilter;
+
+/**
+ * @author Werner Feder (werner.feder@t-online.de)
+ * 
+ */
+public class AddressFilter extends BaseSearchFilter
+{
+
+  private static final long serialVersionUID = -2555263898845331080L;
+
+  public AddressFilter()
+  {
+  }
+
+  public AddressFilter(final BaseSearchFilter filter)
+  {
+    super(filter);
+  }
+}
