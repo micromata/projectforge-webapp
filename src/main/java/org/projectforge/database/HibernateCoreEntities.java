c7f837b549781712e5d5048bfa1620ff636c1c1f
Werner Feder <werner.feder@t-online.de>
2014-04-23 13:28:38 +0200

    Registered AddressKatDO and AddressKatDao.

--- src/main/java/org/projectforge/database/HibernateCoreEntities.java
+++ src/main/java/org/projectforge/database/HibernateCoreEntities.java
@@ -56,6 +56,7 @@ class HibernateCoreEntities
     org.projectforge.access.AccessEntryDO.class, //
     org.projectforge.access.GroupTaskAccessDO.class, //
     org.projectforge.address.AddressDO.class, //
+    org.projectforge.address.contact.AddressKatDO.class, //
     org.projectforge.address.PersonalAddressDO.class, //
     org.projectforge.address.contact.ContactDO.class, //
     org.projectforge.address.contact.ContactEntryDO.class, //
