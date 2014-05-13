71ce57c884370ad03fb24d68951c645a7eff7d8d
Julius Hege <julheg@gmx.de>
2014-05-13 10:44:52 +0200

    Started WORKFLOW for time sheet handling

--- src/main/java/org/projectforge/web/calendar/CalendarForm.java
+++ src/main/java/org/projectforge/web/calendar/CalendarForm.java
@@ -23,22 +23,36 @@
 
 package org.projectforge.web.calendar;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.joda.time.DateMidnight;
+import org.projectforge.timesheet.TimesheetDO;
+import org.projectforge.user.PFUserContext;
 import org.projectforge.user.PFUserDO;
 import org.projectforge.user.UserDao;
 import org.projectforge.user.UserGroupCache;
+import org.projectforge.web.timesheet.TimesheetEditPage;
 import org.projectforge.web.wicket.AbstractStandardForm;
+import org.projectforge.web.wicket.MySession;
 import org.projectforge.web.wicket.WicketUtils;
 import org.projectforge.web.wicket.bootstrap.GridBuilder;
 import org.projectforge.web.wicket.bootstrap.GridSize;
 import org.projectforge.web.wicket.components.DateTimePanel;
 import org.projectforge.web.wicket.components.JodaDatePanel;
 import org.projectforge.web.wicket.flowlayout.ButtonGroupPanel;
+import org.projectforge.web.wicket.flowlayout.ButtonPanel;
+import org.projectforge.web.wicket.flowlayout.ButtonType;
 import org.projectforge.web.wicket.flowlayout.CheckBoxButton;
 import org.projectforge.web.wicket.flowlayout.DivPanel;
 import org.projectforge.web.wicket.flowlayout.DivTextPanel;
@@ -49,6 +63,8 @@ import org.projectforge.web.wicket.flowlayout.IconType;
 public class CalendarForm extends AbstractStandardForm<CalendarFilter, CalendarPage>
 {
   private static final long serialVersionUID = -145923669780937370L;
+
+  final static String WORKFLOW_SESSION_KEY = "workflow_key";
 
   @SpringBean(name = "userGroupCache")
   private UserGroupCache userGroupCache;
@@ -137,7 +153,67 @@ public class CalendarForm extends AbstractStandardForm<CalendarFilter, CalendarP
     }));
     durationLabel = durationPanel.getLabel4Ajax();
     fs.add(durationPanel);
+
+    // workflow toggle button
+    final TimeZone tz = PFUserContext.getTimeZone();
+    final FieldsetPanel workflowFs = gridBuilder.newFieldset("Workflow");
+    workflowFs.setLabelFor(new Label("a","b"));
+    final AjaxButton workflowToggleButton = new AjaxButton(ButtonPanel.BUTTON_ID) {
+
+      @Override
+      protected void onSubmit(final AjaxRequestTarget target, final Form< ? > form)
+      {
+        final TimeZone tz = PFUserContext.getTimeZone();
+        if(MySession.get().getAttribute(WORKFLOW_SESSION_KEY)==null){
+          MySession.get().setAttribute(WORKFLOW_SESSION_KEY, RoundTo5Minutes(Calendar.getInstance(tz)));
+          this.add(AttributeModifier.replace("class", ButtonType.DARK.getClassAttrValue()));
+          this.add(AttributeModifier.replace("title", "TEST"));
+          this.add(AttributeModifier.replace("text", "TEST"));
+          this.add(AttributeModifier.replace("label", "TEST"));
+        }
+        else{
+          final Calendar now = RoundTo5Minutes(Calendar.getInstance(tz));
+          final Calendar oldTime = (Calendar) MySession.get().getAttribute(WORKFLOW_SESSION_KEY);
+          final TimesheetDO timesheetDO = new TimesheetDO();
+          timesheetDO.setStartDate(oldTime.getTimeInMillis());
+          timesheetDO.setStopTime(now.getTimeInMillis());
+          timesheetDO.setDescription("Created by Workflow");
+          final TimesheetEditPage responsePage = new TimesheetEditPage(timesheetDO);
+          responsePage.setReturnToPage((WebPage) getPage());
+          setResponsePage(responsePage);
+          MySession.get().removeAttribute(WORKFLOW_SESSION_KEY);
+        }
+        target.add(this);
+      }
+    };
+    workflowToggleButton.setOutputMarkupId(true);
+    final Calendar oldTime = (Calendar) MySession.get().getAttribute(WORKFLOW_SESSION_KEY);
+    final boolean timeAlreadyStored = (oldTime==null);
+    final Calendar now = RoundTo5Minutes(Calendar.getInstance(tz));
+    String buttonText="";
+    if(!timeAlreadyStored){
+      buttonText = oldTime.get(Calendar.HOUR_OF_DAY)+":"+oldTime.get(Calendar.MINUTE)+"-"+now.get(Calendar.HOUR_OF_DAY)+":"+now.get(Calendar.MINUTE);
+    }
+    workflowFs.add(new ButtonPanel(workflowFs.newChildId(), timeAlreadyStored?"Workflow":(buttonText), workflowToggleButton, timeAlreadyStored?ButtonType.GREEN:ButtonType.DARK));
+
     onAfterInit(gridBuilder);
+  }
+
+  Calendar RoundTo5Minutes(final Calendar date){
+    int minutes = date.get(Calendar.MINUTE);
+    final int seconds = date.get(Calendar.SECOND);
+    /*if((minutes%5==2&&seconds<30)||minutes%5<2){
+      minutes=minutes-(minutes%5);//Round down
+    }
+    else */if((minutes%5==2&&seconds>=30)||minutes%5>2){
+      //minutes-=minutes%5;//Round up
+      minutes+=5;
+    }
+    minutes-=minutes%5;
+    date.set(Calendar.MINUTE,minutes);
+    date.set(Calendar.SECOND, 0);
+    date.set(Calendar.MILLISECOND, 0);
+    return date;
   }
 
   protected String getRefreshIconTooltip()
