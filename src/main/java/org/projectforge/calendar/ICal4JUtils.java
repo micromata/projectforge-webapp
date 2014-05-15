24f245ba9c3145f47206953da8a658ccc5536e79
Werner Feder <werner.feder@t-online.de>
2014-05-15 12:46:27 +0200

    Method getICal now returns string representation of a calendar.

--- src/main/java/org/projectforge/calendar/ICal4JUtils.java
+++ src/main/java/org/projectforge/calendar/ICal4JUtils.java
@@ -30,6 +30,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.ParameterList;
 import net.fortuna.ical4j.model.Recur;
 import net.fortuna.ical4j.model.TimeZone;
@@ -39,12 +40,15 @@ import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.parameter.Cn;
 import net.fortuna.ical4j.model.parameter.PartStat;
 import net.fortuna.ical4j.model.property.Attendee;
+import net.fortuna.ical4j.model.property.CalScale;
 import net.fortuna.ical4j.model.property.Comment;
 import net.fortuna.ical4j.model.property.Contact;
 import net.fortuna.ical4j.model.property.Location;
 import net.fortuna.ical4j.model.property.Organizer;
+import net.fortuna.ical4j.model.property.ProdId;
 import net.fortuna.ical4j.model.property.RRule;
 import net.fortuna.ical4j.model.property.Uid;
+import net.fortuna.ical4j.model.property.Version;
 import net.fortuna.ical4j.util.Dates;
 
 import org.apache.commons.lang.StringUtils;
@@ -401,24 +405,23 @@ public class ICal4JUtils
 
   public static String getICal(final TeamEventDO teamEvent) {
     final StringBuffer buf = new StringBuffer();
-
+    final Calendar calendar = new Calendar();
+    calendar.getProperties().add(new ProdId("-//Ben Fortuna//iCal4j 1.0//EN"));
+    calendar.getProperties().add(Version.VERSION_2_0);
+    calendar.getProperties().add(CalScale.GREGORIAN);
     final VEvent vEvent = ICal4JUtils.createVEvent(teamEvent.getStartDate(), teamEvent.getEndDate(), teamEvent.getUid(), teamEvent.getSubject(), teamEvent.isAllDay());
-
     if (teamEvent.hasRecurrence() == true) {
       vEvent.getProperties().add(new RRule(teamEvent.getRecurrenceObject()));
     }
-
     if (StringUtils.isNotBlank(teamEvent.getLocation()) == true) {
       vEvent.getProperties().add(new Location(teamEvent.getLocation()));
     }
     if (StringUtils.isNotBlank(teamEvent.getNote()) == true) {
       vEvent.getProperties().add(new Comment(teamEvent.getNote()));
     }
-
     final PFUserDO user = PFUserContext.getUser();
     final String s = user.getFullname() + "\\, " + user.getOrganization() + "\\, " + user.getPersonalPhoneIdentifiers();
     vEvent.getProperties().add(new Contact(s.intern()));
-
     try {
       if (StringUtils.isNotBlank(user.getEmail()) == true) {
         final ParameterList organizerParams = new ParameterList();
@@ -429,9 +432,7 @@ public class ICal4JUtils
     } catch (final Exception e) {
       log.error("Cant't build organizer " + e.getMessage());
     }
-
     if (teamEvent.getAttendees() != null) {
-
       for (final TeamEventAttendeeDO attendee : teamEvent.getAttendees() ) {
         final ParameterList attendeeParams = new ParameterList();
         if (attendee.getUser() != null) {
@@ -439,8 +440,6 @@ public class ICal4JUtils
             attendeeParams.add(new Cn(attendee.getUser().getFullname()));
             attendeeParams.add(new PartStat(attendee.getStatus().name()));
             vEvent.getProperties().add(new Attendee(attendeeParams, attendee.getUser().getEmail()));
-            //            attendeeParams.removeAll(attendee.getUser().getFullname());
-            //            attendeeParams.removeAll(attendee.getStatus().name());
           } catch (final Exception e) {
             log.error("Cant't build attendee " + e.getMessage());
           }
@@ -448,15 +447,14 @@ public class ICal4JUtils
           try {
             attendeeParams.add(new PartStat(attendee.getStatus().name()));
             vEvent.getProperties().add(new Attendee(attendeeParams, attendee.getUrl()));
-            //            attendeeParams.removeAll(attendee.getStatus().name());
           }  catch (final Exception e) {
             log.error("Cant't build attendee " + e.getMessage());
           }
         }
       }
     }
-
-    buf.append(vEvent.toString());
+    calendar.getComponents().add(vEvent);
+    buf.append(calendar.toString());
     return buf.toString();
   }
 }
